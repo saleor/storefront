05e8bcb71dc2808649605b60cc83dc8dce5aa560
Zaiste <oh@zaiste.net>
2021-08-04 14:39:11 +0200

    pagintion

--- components/ProductCollection.tsx
+++ components/ProductCollection.tsx
@@ -1,10 +1,15 @@
+import React, { useState } from 'react';
 import {
-  useLatestProductsQuery, useTShirtProductsQuery, useFilterProductsQuery, OrderDirection, ProductOrderField,
+  useProductsQuery, useTShirtProductsQuery, useFilterProductsQuery, OrderDirection, ProductOrderField,
   useProductByIdQuery,
 } from '../generated/graphql';
 
 function Products() {
-  const { loading, error, data } = useLatestProductsQuery();
+  const [before, setBefore] = useState('');
+  const [after, setAfter] = useState('');
+  const { loading, error, data } = useProductsQuery({
+    variables: { after, before }
+  });
   // const { loading, error, data } = useFilterProductsQuery({
   //   variables: {
   //     filter: { search: 'T-Shirt' },
@@ -24,20 +29,24 @@ function Products() {
     const latestProducts = data.products?.edges || [];
 
     return (
-      <ul role="list" className="grid gap-4 grid-cols-4">
-        {latestProducts?.length > 0 &&
-          latestProducts.map(
-            ({ node: { id, name, thumbnail, category } }) => (
-              <li key={id} className="relative bg-white">
-                <img src={thumbnail?.url} alt="" />
-                <div className="p-2 border-gray-100 border-t">
-                  <p className="block text-lg text-gray-900 truncate">{name}</p>
-                  <p className="block text-sm font-medium text-gray-500">{category?.name}</p>
-                </div>
-              </li>
-            ),
-          )}
-      </ul>
+      <div>
+        <ul role="list" className="grid gap-4 grid-cols-4">
+          {latestProducts?.length > 0 &&
+            latestProducts.map(
+              ({ node: { id, name, thumbnail, category } }) => (
+                <li key={id} className="relative bg-white">
+                  <img src={thumbnail?.url} alt="" />
+                  <div className="p-2 border-gray-100 border-t">
+                    <p className="block text-lg text-gray-900 truncate">{name}</p>
+                    <p className="block text-sm font-medium text-gray-500">{category?.name}</p>
+                  </div>
+                </li>
+              ),
+            )}
+        </ul>
+        <a href="#" onClick={() => setBefore(latestProducts[0].cursor || '')}>Prev</a>
+        <a href="#" onClick={() => setAfter(latestProducts[latestProducts.length - 1].cursor || '')}>Next</a>
+      </div>
     );
   }
 
