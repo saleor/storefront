d0a25ca7a8948faff9376232646559239baca591
Zaiste <oh@zaiste.net>
2021-08-04 11:53:07 +0200

    filter by `search`

--- components/ProductCollection.tsx
+++ components/ProductCollection.tsx
@@ -1,33 +1,34 @@
-import { useLatestProductsQuery } from '../generated/graphql';
+import { useLatestProductsQuery, useTShirtProductsQuery } from '../generated/graphql';
 
 function Products() {
-	const { loading, error, data } = useLatestProductsQuery();
+  // const { loading, error, data } = useLatestProductsQuery();
+  const { loading, error, data } = useTShirtProductsQuery();
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
 
-	if (data) {
-		const latestProducts = data.products?.edges || [];
+  if (data) {
+    const latestProducts = data.products?.edges || [];
 
-		return (
-			<ul role="list" className="grid gap-4 grid-cols-4">
-				{latestProducts?.length > 0 &&
-					latestProducts.map(
-						({ node: { id, name, thumbnail, category } }) => (
-							<li key={id} className="relative bg-white">
-								<img src={thumbnail.url} alt="" />
-								<div className="p-2 border-gray-100 border-t">
-									<p className="block text-lg text-gray-900 truncate">{name}</p>
-									<p className="block text-sm font-medium text-gray-500">{category?.name}</p>
-								</div>
-							</li>
-						),
-					)}
-			</ul>
-		);
-	}
+    return (
+      <ul role="list" className="grid gap-4 grid-cols-4">
+        {latestProducts?.length > 0 &&
+          latestProducts.map(
+            ({ node: { id, name, thumbnail, category } }) => (
+              <li key={id} className="relative bg-white">
+                <img src={thumbnail?.url} alt="" />
+                <div className="p-2 border-gray-100 border-t">
+                  <p className="block text-lg text-gray-900 truncate">{name}</p>
+                  <p className="block text-sm font-medium text-gray-500">{category?.name}</p>
+                </div>
+              </li>
+            ),
+          )}
+      </ul>
+    );
+  }
 
-	return null;
+  return null;
 }
 
 export default Products;
