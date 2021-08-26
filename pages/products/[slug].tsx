eabd037aff49ede400166d62461fdfb81aa66667
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-08-26 17:12:01 +0200

    Use product slug for details URL and add variant selection

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { useRouter } from "next/router";
 import { ApolloQueryResult } from "@apollo/client";
 import Blocks from "editorjs-blocks-react-renderer";
@@ -6,99 +6,124 @@ import Blocks from "editorjs-blocks-react-renderer";
 import { Navbar } from "../../components/Navbar";
 import {
   useAddProductToCheckoutMutation,
-  useProductByIdQuery,
   Product,
   ProductPathsQuery,
+  useProductBySlugQuery,
 } from "../../saleor/api";
 import { ProductPaths } from "../../components/config";
 import apolloClient from "../../lib/graphql";
 import { formatAsMoney } from "../../lib/utils";
+import Link from "next/link";
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  return {
+    props: {
+      productSlug: context.params?.slug?.toString(),
+      token: "",
+    },
+  };
+};
 
 export default function ProductPage({
-  product,
+  productSlug,
   token,
-}: {
-  product: Product;
-  token: string;
-}) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  const { loading, error, data } = useProductByIdQuery({ variables: product });
+  const { loading, error, data } = useProductBySlugQuery({
+    variables: { slug: productSlug || "" },
+  });
   const [addProductToCheckout] = useAddProductToCheckoutMutation();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
+  if (!data) {
+    return null;
+  }
 
-  if (data) {
-    const { product } = data;
-    const price = product?.pricing?.priceRange?.start?.gross.amount || 0;
-    const variantId = product?.variants![0]!.id!;
+  const { product } = data;
+  const price = product?.pricing?.priceRange?.start?.gross.amount || 0;
 
-    return (
-      <div className="min-h-screen bg-gray-100">
-        <Navbar />
+  const selectedVariantId =
+    router.query.variant?.toString() || product?.variants![0]!.id!;
 
-        <main className="max-w-7xl mx-auto pt-8 px-8">
-          <div className="grid grid-cols-2 gap-x-10 items-start">
-            <div className="w-full aspect-w-1 aspect-h-1 bg-white rounded">
-              <img
-                src={product?.media![0]?.url}
-                className="w-full h-full object-center object-cover"
-              />
+  const onAddToCart = async () => {
+    await addProductToCheckout({
+      variables: { checkoutId: token, variantId: selectedVariantId },
+    });
+    router.push("/cart");
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+
+      <main className="max-w-7xl mx-auto pt-8 px-8">
+        <div className="grid grid-cols-2 gap-x-10 items-start">
+          <div className="w-full aspect-w-1 aspect-h-1 bg-white rounded">
+            <img
+              src={product?.media![0]?.url}
+              className="w-full h-full object-center object-cover"
+            />
+          </div>
+
+          <div className="space-y-8">
+            <div>
+              <h1 className="text-4xl font-bold tracking-tight text-gray-800">
+                {product?.name}
+              </h1>
+              <p className="text-lg mt-2 font-medium text-gray-500">
+                {product?.category?.name}
+              </p>
             </div>
 
-            <div className="space-y-8">
-              <div>
-                <h1 className="text-4xl font-bold tracking-tight text-gray-800">
-                  {product?.name}
-                </h1>
-                <p className="text-lg mt-2 font-medium text-gray-500">
-                  {product?.category?.name}
-                </p>
+            <p className="text-2xl text-gray-900">{formatAsMoney(price)}</p>
+
+            {!!product?.description && (
+              <div className="text-base text-gray-700 space-y-6">
+                <article className="prose lg:prose-s">
+                  <Blocks data={JSON.parse(product?.description)} />
+                </article>
               </div>
-
-              <p className="text-2xl text-gray-900">{formatAsMoney(price)}</p>
-
-              {!!product?.description && (
-                <div className="text-base text-gray-700 space-y-6">
-                  <article className="prose lg:prose-s">
-                    <Blocks data={JSON.parse(product?.description)} />
-                  </article>
-                </div>
-              )}
-              <div className="grid grid-cols-8 gap-2">
-                {product?.variants?.map((variant) => {
-                  return (
+            )}
+            <div className="grid grid-cols-8 gap-2">
+              {product?.variants?.map((variant) => {
+                return (
+                  <Link
+                    key={variant?.name}
+                    href={{
+                      pathname: "/products/[slug]",
+                      query: { variant: variant?.id, slug: productSlug },
+                    }}
+                    replace
+                    shallow
+                  >
                     <a
-                      key={variant?.name}
-                      className="flex justify-center border border-gray-300 rounded-md p-3 font-semibold hover:border-blue-300"
+                      className={`flex justify-center border rounded-md p-3 font-semibold hover:border-blue-400 ${
+                        variant?.id === selectedVariantId
+                          ? "border-blue-300"
+                          : "border-gray-300"
+                      }`}
                     >
                       {variant?.name}
                     </a>
-                  );
-                })}
-              </div>
-
-              <button
-                onClick={async () => {
-                  await addProductToCheckout({
-                    variables: { checkoutId: token, variantId },
-                  });
-                  router.push("/cart");
-                }}
-                type="submit"
-                className="max-w-xs w-full bg-blue-500 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-white hover:bg-blue-600 focus:outline-none"
-              >
-                Add to cart
-              </button>
+                  </Link>
+                );
+              })}
             </div>
-          </div>
-        </main>
-      </div>
-    );
-  }
 
-  return null;
+            <button
+              onClick={onAddToCart}
+              type="submit"
+              className="max-w-xs w-full bg-blue-500 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-white hover:bg-blue-600 focus:outline-none"
+            >
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export async function getStaticPaths() {
@@ -109,7 +134,7 @@ export async function getStaticPaths() {
     });
   const paths = !!result
     ? result.data?.products?.edges.map(({ node }) => ({
-        params: { slug: node.id },
+        params: { slug: node.slug },
       }))
     : [];
 
@@ -118,13 +143,3 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
-
-export const getStaticProps: GetStaticProps<any> = async ({ params }) => {
-  const product = { id: params?.slug };
-
-  return {
-    props: {
-      product,
-    },
-  };
-};
