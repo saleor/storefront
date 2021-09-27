252f6734b1d8f5bc52e87397b2b8d30daae3ac80
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-09-27 21:06:06 +0200

    Add SEO to the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import BaseTemplate from "@/components/BaseTemplate";
 import HomepageBlock from "@/components/HomepageBlock";
+import BaseSeo from "@/components/seo/BaseSeo";
 import { useMenuQuery } from "@/saleor/api";
 import React from "react";
 
@@ -7,11 +8,16 @@ const Home: React.VFC = () => {
   const { data, loading } = useMenuQuery({ variables: { slug: "homepage" } });
 
   if (loading) {
-    return <BaseTemplate isLoading={true} />;
+    return (
+      <BaseTemplate isLoading={true}>
+        <BaseSeo />
+      </BaseTemplate>
+    );
   }
 
   return (
     <BaseTemplate>
+      <BaseSeo />
       <div className="py-10">
         <header className="mb-4">
           <div className="max-w-7xl mx-auto px-8"></div>
