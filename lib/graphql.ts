33bbf41d144793b919f317043e72a512e6ad8043
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-09-27 12:47:17 +0200

    Use local schema for extensions

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -1,9 +1,30 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
 import { typePolicies } from "./typePolicies";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "https://vercel.saleor.cloud/graphql/",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  let token = "";
+  if (process.browser) {
+    // Client-side-only code
+    token = localStorage.getItem("saleorAuthToken") || "";
+  }
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const apolloClient = new ApolloClient({
-  uri: "https://vercel.saleor.cloud/graphql/",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({ typePolicies }),
   ssrMode: typeof window === "undefined",
 });
