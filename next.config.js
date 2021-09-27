beb69bb41e00747d2662621242e9087799ca0610
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-09-28 00:20:16 +0200

    Use preact for prod build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ["vercel.saleor.cloud"],
   },
-};
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
+});
