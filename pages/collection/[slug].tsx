3d6b9e5be733565dda6d7181b29b43c941648e00
Irgen Kini <irgenkini@Irgens-MacBook-Pro.local>
2021-10-06 12:34:26 +0200

    Collection View

---
+++ pages/collection/[slug].tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { ApolloQueryResult } from "@apollo/client";
+import apolloClient from "@/lib/graphql";
+import {
+  CollectionPathsDocument,
+  CollectionPathsQuery,
+  useCollectionBySlugQuery,
+} from "@/saleor/api";
+import BaseTemplate from "@/components/BaseTemplate";
+import Custom404 from "pages/404";
+import { ProductCollection } from "@/components";
+import CollectionPageSeo from "@/components/seo/CollectionPageSeo";
+import PageHero from "@/components/PageHero";
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  return {
+    props: {
+      collectionSlug: context.params?.slug?.toString(),
+    },
+  };
+};
+
+const CollectionPage: React.VFC<
+  InferGetStaticPropsType<typeof getStaticProps>
+> = ({ collectionSlug }) => {
+  const {
+    loading,
+    error,
+    data: collectionData,
+  } = useCollectionBySlugQuery({
+    variables: { slug: collectionSlug || "" },
+    skip: !collectionSlug,
+  });
+
+  if (loading) {
+    return <BaseTemplate isLoading={true} />;
+  }
+  if (error) return <p>Error {error.message}</p>;
+
+  const collection = collectionData?.collection;
+
+  if (!collection) {
+    return <Custom404 />;
+  }
+  return (
+    <BaseTemplate>
+      <CollectionPageSeo collection={collection} />
+      <header className="mb-4 pt-4">
+        <div className="max-w-7xl mx-auto px-8">
+          <PageHero entity={collection} />
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto px-8">
+          <ProductCollection filter={{ collections: [collection?.id] }} />
+        </div>
+      </main>
+    </BaseTemplate>
+  );
+};
+
+export default CollectionPage;
+
+export async function getStaticPaths() {
+  const result: ApolloQueryResult<CollectionPathsQuery | undefined> =
+    await apolloClient.query({
+      query: CollectionPathsDocument,
+      variables: {},
+    });
+
+  const paths = result.data?.collections?.edges.map(({ node }) => ({
+    params: { slug: node.slug },
+  }));
+
+  return {
+    paths: paths,
+    fallback: true,
+  };
+}
