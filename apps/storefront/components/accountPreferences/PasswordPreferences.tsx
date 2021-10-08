e886cf00a680ba014e02e556daab4e7ea4c32a7e
Irgen Kini <irgenkini@Irgens-MacBook-Pro.local>
2021-10-08 11:39:37 +0200

    account preferences rebase

--- apps/storefront/components/accountPreferences/PasswordPreferences.tsx
+++ apps/storefront/components/accountPreferences/PasswordPreferences.tsx
@@ -1,7 +1,13 @@
+import { usePasswordChangeMutation } from "@/saleor/api";
 import React from "react";
 import { useForm } from "react-hook-form";
 
 export const PasswordPreferences: React.VFC<any> = ({}) => {
+  const [passwordChangeMutation] = usePasswordChangeMutation({});
+  const [successMessage, setSuccessMessage] = React.useState<String | null>(
+    null
+  );
+
   const {
     register,
     handleSubmit,
@@ -10,28 +16,31 @@ export const PasswordPreferences: React.VFC<any> = ({}) => {
   } = useForm();
 
   const onPasswordPreferenceSubmit = handleSubmit(async (formData) => {
-    // const result = await requestEmailChange({
-    //   variables: {
-    //     channel: "default-channel",
-    //     newEmail: formData.email,
-    //     password: "IrgenKini98!",
-    //     redirectUrl: "/",
-    //   },
-    // });
-    // const errors = result.data?.errors || [];
-    // if (errors.length > 0) {
-    //   errors.forEach((e) => setError("email", { message: e.message || "" }));
-    //   return;
-    // }
+    if (formData.newPassword !== formData.newPasswordRepeat) {
+      setError("error", { message: "Passwords have to match." });
+    } else {
+      const result = await passwordChangeMutation({
+        variables: {
+          newPassword: formData.newPassword,
+          oldPassword: formData.oldPassword,
+        },
+      });
+      const errors = result.data?.passwordChange?.errors || [];
+      if (errors.length > 0) {
+        errors.forEach((e) => setError("error", { message: e.message || "" }));
+        return;
+      } else if (result.data?.passwordChange?.user) {
+        setSuccessMessage("Password changed succesfully.");
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 5000);
+      }
+    }
   });
   return (
     <div className="mt-4 mb-4">
       <h2 className="checkout-section-header-active mb-2">Change password</h2>
-      <form
-        onSubmit={() => {
-          console.log("whatever");
-        }}
-      >
+      <form onSubmit={onPasswordPreferenceSubmit}>
         <div className="grid grid-cols-12 gap-4 w-full">
           <div className="col-span-full">
             <input
@@ -42,7 +51,6 @@ export const PasswordPreferences: React.VFC<any> = ({}) => {
                 required: true,
               })}
             />
-            <p>{errors.email?.message}</p>
           </div>
         </div>
         <div className="grid grid-cols-12 gap-4 w-full mt-2">
@@ -55,13 +63,28 @@ export const PasswordPreferences: React.VFC<any> = ({}) => {
                 required: true,
               })}
             />
-            <p>{errors.email?.message}</p>
           </div>
         </div>
+        <div className="grid grid-cols-12 gap-4 w-full mt-2">
+          <div className="col-span-full">
+            <input
+              className="px-4 py-2 rounded-md text-sm outline-none w-full"
+              type="password"
+              placeholder="Repeat new Password"
+              {...register("newPasswordRepeat", {
+                required: true,
+              })}
+            />
+            <p className="mt-2 text-sm text-red-600">{errors.error?.message}</p>
+          </div>
+        </div>
+        {successMessage !== null ? (
+          <p className="mt-2 text-sm text-green-600">{successMessage}</p>
+        ) : null}
         <div>
           <button
-            className="mt-4 w-40 bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100"
-            onClick={() => console.log("whatever")}
+            className="mt-2 w-40 bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100"
+            onClick={() => onPasswordPreferenceSubmit()}
           >
             Submit changes
           </button>
