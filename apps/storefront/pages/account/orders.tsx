3ab9b13d8922953af748c91a83c2be5b25faa0d1
Irgen Kini <irgenkini@Irgens-MacBook-Pro.local>
2021-10-11 12:45:49 +0200

    Navigation Bar

---
+++ apps/storefront/pages/account/orders.tsx
@@ -0,0 +1,24 @@
+import BaseTemplate from "@/components/BaseTemplate";
+import { NavigationPanel } from "@/components/NavigationPanel";
+
+const OrdersPage: React.VFC = () => {
+  return (
+    <BaseTemplate>
+      <div className="py-10">
+        <header className="mb-4">
+          <h1 className="max-w-7xl text-2xl mx-auto px-8">Account</h1>
+        </header>
+        <main className="flex max-w-7xl mx-auto px-8">
+          <div className="flex-initial w-2/5">
+            <NavigationPanel active={"Orders"} />
+          </div>
+          <div className="border-r flex flex-auto flex-col overflow-y-auto px-4 pt-4 space-y-4 pb-4">
+            <div>Orders Component goes here</div>
+          </div>
+        </main>
+      </div>
+    </BaseTemplate>
+  );
+};
+
+export default OrdersPage;
