893abf6f7005376d99ac227d9421138c60a04f52
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-10-25 12:17:29 +0200

    Update dependencies + bundle analyzer (#43)

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -1,10 +1,15 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+
   images: {
     domains: ["vercel.saleor.cloud"],
   },
   i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US'
-  }
-};
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+});
