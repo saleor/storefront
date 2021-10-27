d9db995e87a7301ea247748195f6229eefd2c892
Irgen Kini <irgen.kini@tango.agency>
2021-10-27 17:35:33 +0200

    Checkout Container Alignment Fix

--- components/checkout/sidebar/CheckoutSidebar.tsx
+++ components/checkout/sidebar/CheckoutSidebar.tsx
@@ -13,7 +13,7 @@ export const CheckoutSidebar: React.VFC<CheckoutSidebarProps> = ({
   const lines = checkout.lines?.filter((l) => !!l) || [];
   return (
     <section className="max-w-md w-full flex flex-col ">
-      <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 p-4">
+      <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 md:pr-4 md:py-4 md:pl-0 p-4">
         Order summary
       </h1>
 
