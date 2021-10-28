ec2e871073fedad0974bb5e93299cdf69ca2ae31
Irgen Kini <irgen.kini@tango.agency>
2021-10-28 16:38:22 +0200

    Product image gallery - image expanded full screen

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -24,6 +24,7 @@ import {
   useCreateCheckoutMutation,
 } from "@/saleor/api";
 import { ImageExpand } from "@/components/ImageExpand";
+import clsx from "clsx";
 
 const ProductPage = ({
   productSSG,
@@ -124,75 +125,98 @@ const ProductPage = ({
   const productImage = product?.media![0];
 
   return (
-    <BaseTemplate>
-      <ProductPageSeo product={product} />
+    <>
+      <BaseTemplate>
+        <ProductPageSeo product={product} />
 
-      <main className="grid grid-cols-1 md:grid-cols-3 gap-4 max-h-screen overflow-auto md:overflow-hidden max-w-7xl mx-auto pt-8 px-8">
-        <div
-          className="md:col-span-2 mt-1 mb-2 w-full max-h-screen overflow-auto grid grid-cols-1 md:grid-cols-2 md:h-full h-96 overflow-auto"
-          style={{ scrollSnapType: "both mandatory" }}
+        <main
+          className={clsx(
+            "grid grid-cols-1 gap-4 max-h-full overflow-auto md:overflow-hidden max-w-7xl mx-auto pt-8 px-8",
+            product.media && product.media.length > 1 && "md:grid-cols-3",
+            product.media && product.media.length === 1 && "md:grid-cols-2",
+            expandedImage && "hidden"
+          )}
         >
-          {product.media?.map((media) => {
-            return (
-              <div
-                key={media.url}
-                className={"aspect-w-1 aspect-h-1"}
-                onClick={() => setExpandedImage(media)}
-              >
-                <Image
-                  src={media.url}
-                  alt={media.alt}
-                  layout="fill"
-                  objectFit="cover"
-                ></Image>
-              </div>
-            );
-          })}
-        </div>
-
-        <div className="space-y-8 mt-10 md:mt-0">
-          <div>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-800">
-              {product?.name}
-            </h1>
-            <Link href={`/category/${product?.category?.slug}`} passHref>
-              <p className="text-lg mt-2 font-medium text-gray-500 cursor-pointer">
-                {product?.category?.name}
-              </p>
-            </Link>
+          <div
+            className={clsx(
+              "mt-1 mb-2 w-full max-h-screen overflow-scroll grid grid-cols-1 md:h-full h-96",
+              product.media &&
+                product.media.length > 1 &&
+                "md:grid-cols-2 md:col-span-2"
+            )}
+            style={{
+              scrollSnapType: "both mandatory",
+            }}
+          >
+            {product.media?.map((media) => {
+              return (
+                <div
+                  key={media.url}
+                  className={"aspect-w-1 aspect-h-1"}
+                  onClick={() => setExpandedImage(media)}
+                  style={{
+                    scrollSnapAlign: "start",
+                  }}
+                >
+                  <Image
+                    src={media.url}
+                    alt={media.alt}
+                    layout="fill"
+                    objectFit="cover"
+                  ></Image>
+                </div>
+              );
+            })}
           </div>
 
-          <p className="text-2xl text-gray-900">{price}</p>
-
-          {product?.description && (
-            <div className="text-base text-gray-700 space-y-6">
-              <RichText jsonStringData={product.description} />
+          <div className="space-y-8 mt-10 md:mt-0">
+            <div>
+              <h1 className="text-4xl font-bold tracking-tight text-gray-800">
+                {product?.name}
+              </h1>
+              <Link href={`/category/${product?.category?.slug}`} passHref>
+                <p className="text-lg mt-2 font-medium text-gray-500 cursor-pointer">
+                  {product?.category?.name}
+                </p>
+              </Link>
             </div>
-          )}
-          <VariantSelector
-            product={product}
-            selectedVariantID={selectedVariantID}
+
+            <p className="text-2xl text-gray-900">{price}</p>
+
+            {product?.description && (
+              <div className="text-base text-gray-700 space-y-6">
+                <RichText jsonStringData={product.description} />
+              </div>
+            )}
+            <VariantSelector
+              product={product}
+              selectedVariantID={selectedVariantID}
+            />
+            {selectedVariant && selectedVariant?.quantityAvailable > 0 ? (
+              <button
+                onClick={onAddToCart}
+                type="submit"
+                disabled={loadingAddToCheckout}
+                className="max-w-xs w-full bg-blue-500 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-white hover:bg-blue-600 focus:outline-none"
+              >
+                {loadingAddToCheckout ? "Adding..." : "Add to cart"}
+              </button>
+            ) : (
+              <p className="text-lg- text-yellow-600">Sold out!</p>
+            )}
+            {!!addToCartError && <p>{addToCartError}</p>}
+          </div>
+        </main>
+      </BaseTemplate>
+      {expandedImage && (
+        <div className="absolute min-h-screen min-w-screen h-full w-full top-0 bottom-0 left-0 right-0 z-40">
+          <ImageExpand
+            image={expandedImage}
+            onRemoveExpand={() => setExpandedImage(undefined)}
           />
-          {selectedVariant && selectedVariant?.quantityAvailable > 0 ? (
-            <button
-              onClick={onAddToCart}
-              type="submit"
-              disabled={loadingAddToCheckout}
-              className="max-w-xs w-full bg-blue-500 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-white hover:bg-blue-600 focus:outline-none"
-            >
-              {loadingAddToCheckout ? "Adding..." : "Add to cart"}
-            </button>
-          ) : (
-            <p className="text-lg- text-yellow-600">Sold out!</p>
-          )}
-          {!!addToCartError && <p>{addToCartError}</p>}
         </div>
-        <ImageExpand
-          image={expandedImage}
-          onRemoveExpand={() => setExpandedImage(undefined)}
-        />
-      </main>
-    </BaseTemplate>
+      )}
+    </>
   );
 };
 
