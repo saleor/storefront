703df4b84ba6c04c1ac3d994f89b6ecf9b0f977b
Irgen Kini <irgen.kini@tango.agency>
2021-10-28 16:38:22 +0200

    Product image gallery - image expanded full screen

--- apps/storefront/components/ImageExpand.tsx
+++ apps/storefront/components/ImageExpand.tsx
@@ -12,11 +12,11 @@ export const ImageExpand = ({ image, onRemoveExpand }: ImageExpandProps) => {
       {image && (
         <div
           className={
-            "min-h-screen absolute max-w-7xl grid grid-cols-1 mx-auto px-8 md:h-full w-full bg-gray-100"
+            "min-h-screen absolute grid grid-cols-1 mx-auto px-8 md:h-full w-full bg-gray-100"
           }
         >
           <div
-            className="absolute grid h-6 justify-end w-full z-40 px-2 lg:px-8 mx-auto"
+            className="absolute grid h-6 justify-end w-full z-40 p-8 lg:px-8 mx-auto"
             onClick={() => onRemoveExpand()}
           >
             <svg
@@ -34,7 +34,7 @@ export const ImageExpand = ({ image, onRemoveExpand }: ImageExpandProps) => {
               />
             </svg>
           </div>
-          <div className="w-full h-4/5 max-h-6xl absolute">
+          <div className="w-full h-full absolute md:mt-10">
             <Image src={image.url} layout="fill" objectFit="scale-down"></Image>
           </div>
         </div>
