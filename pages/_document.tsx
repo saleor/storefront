f112c7c9330a27c94caaaf50ba30f49126caa849
Zaiste <oh@zaiste.net>
2022-01-24 12:35:10 -0800

    refactor pre links to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,15 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
+    const uri = process.env.NEXT_PUBLIC_API_URI!;
+    const { hostname } = new URL(uri);
+
     return (
       <Html lang="en" className="bg-gray-50">
-        <Head />
+        <Head>
+          <link rel="preconnect" href={`//${hostname}`} crossOrigin="true" />
+          <link rel="dns-prefetch" href={`//${hostname}`} />
+        </Head>
         <body>
           <Main />
           <NextScript />
