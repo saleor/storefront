1e1e20a79f4facd76144e65afc87300b9370c9bd
Jakub Neander <oh@zaiste.net>
2022-01-24 12:42:06 -0800

    refactor pre links to _document.tsx (#127)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
-import Head from "next/head";
 import React, { ReactElement, ReactNode } from "react";
 
 import RegionsProvider from "@/components/RegionsProvider";
@@ -22,28 +21,13 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
 
   return (
-    <>
-      <Head>
-        {/*
-          Adding preconnect/dns-prefetch allow us to not wait for dns resolving
-          when contacting the Saleor API for the first time.
-          https://developer.mozilla.org/en-US/docs/Web/Performance/dns-prefetch
-        */}
-        <link
-          rel="preconnect"
-          href={process.env.NEXT_PUBLIC_API_URI}
-          crossOrigin="true"
-        />
-        <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_API_URI} />
-      </Head>
-      <RegionsProvider>
-        <ApolloProvider client={apolloClient}>
-          <SaleorProviderWithChannels>
-            {getLayout(<Component {...pageProps} />)}
-          </SaleorProviderWithChannels>
-        </ApolloProvider>
-      </RegionsProvider>
-    </>
+    <RegionsProvider>
+      <ApolloProvider client={apolloClient}>
+        <SaleorProviderWithChannels>
+          {getLayout(<Component {...pageProps} />)}
+        </SaleorProviderWithChannels>
+      </ApolloProvider>
+    </RegionsProvider>
   );
 };
 
