0a6edfa8dd1f4b590f14040d72932aec30d33f27
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-02-16 15:32:46 +0100

    Fix scrollbar display on the gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,6 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
+    require('tailwind-scrollbar-hide'),
   ],
 };
