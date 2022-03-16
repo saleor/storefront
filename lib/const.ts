6d8231118230202297c96f7266a8649b8c286d16
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-03-16 10:32:44 +0100

    Make homepage menu configurable

--- lib/const.ts
+++ lib/const.ts
@@ -1,3 +1,4 @@
 export const CHECKOUT_TOKEN = "checkoutToken";
 export const API_URI = process.env.NEXT_PUBLIC_API_URI || "";
 export const DEMO_MODE = process.env.NEXT_PUBLIC_DEMO_MODE === "true";
+export const HOMEPAGE_MENU = process.env.NEXT_PUBLIC_HOMEPAGE_MENU || "";
