8f50a382657c049a9f27deec4b245940b43c8a8e
Jakub Majorek <majorek.jakub@gmail.com>
2022-03-25 09:44:34 +0100

    Add security headers

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -22,6 +22,25 @@ module.exports = withBundleAnalyzer({
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "x-content-type-options",
+            value: "nosniff",
+          },
+          { key: "x-xss-protection", value: "1" },
+          { key: "x-frame-options", value: "DENY" },
+          {
+            key: "strict-transport-security",
+            value: "max-age=31536000; includeSubDomains",
+          },
+        ],
+      },
+    ];
+  },
   experimental: {
     reactRoot: true,
   },
