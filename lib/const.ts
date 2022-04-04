ea8dacbcda09492d04430d0d51bc60a847c97b64
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-04-04 20:33:44 +0200

    Add geolocation example

--- lib/const.ts
+++ lib/const.ts
@@ -2,3 +2,4 @@ export const CHECKOUT_TOKEN = "checkoutToken";
 export const API_URI = process.env.NEXT_PUBLIC_API_URI || "";
 export const DEMO_MODE = process.env.NEXT_PUBLIC_DEMO_MODE === "true";
 export const HOMEPAGE_MENU = process.env.NEXT_PUBLIC_HOMEPAGE_MENU || "";
+export const GEOLOCATION = process.env.NEXT_PUBLIC_GEOLOCATION === "true";
