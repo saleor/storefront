763fded0fa4f5976702d19d92d8b03111a168750
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-04-06 10:48:12 +0200

    Use headless component for the modal

--- apps/storefront/components/RegionDialog/RegionDialog.tsx
+++ apps/storefront/components/RegionDialog/RegionDialog.tsx
@@ -1,3 +1,4 @@
+import { Dialog } from "@headlessui/react";
 import { useRouter } from "next/router";
 import React from "react";
 import { useForm } from "react-hook-form";
@@ -54,56 +55,68 @@ export function RegionDialog({ isOpen, onClose }: RegionDialogProps) {
   }
 
   return (
-    <div
-      className="min-w-screen h-screen fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center"
-      id="modal-id"
-    >
-      <div className="absolute bg-black opacity-50 inset-0 z-0" />
-      <div className="w-full  max-w-lg pb-6 relative mx-auto my-auto rounded-sm shadow-lg flex flex-col items-center bg-white ">
-        <div className="text-center p-5 mt-4 flex-auto justify-center">
-          <h2 className="text-2xl font-bold py-4">{t.formatMessage(messages.regionModalHeader)}</h2>
-        </div>
-        <div className="text-gray-300 py-1 mb-4 flex flex-col items-center w-3/5">
-          <p className="text-sm text-gray-500 self-start mb-1">
-            {t.formatMessage(messages.channelFieldLabel)}
-          </p>
-          <select
-            className="w-full text-black"
-            id="channel"
-            {...register("channel", {
-              required: true,
-            })}
-          >
-            {channels.map(({ slug, name }) => (
-              <option key={slug} value={slug}>
-                {name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="text-gray-300 py-1 pb-3 flex flex-col items-center w-3/5">
-          <p className="text-sm text-gray-500 self-start mb-1">
-            {t.formatMessage(messages.languageFieldLabel)}
-          </p>
-          <select
-            className="w-full text-black"
-            id="locale"
-            {...register("locale", {
-              required: true,
-            })}
-          >
-            {LOCALES.map((locale) => (
-              <option key={locale.slug} value={locale.slug}>
-                {locale.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="p-3  mt-4 mb-4 text-center space-x-4 md:block">
-          <Button onClick={onSubmit}>{t.formatMessage(messages.saveButton)}</Button>
+    <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" open={isOpen} onClose={onClose}>
+      <div className="min-h-screen px-4 text-center flex flex-col items-center justify-center">
+        <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+
+        <div className="inline-block w-full max-w-xl p-6 my-8 overflow-hidden align-middle transition-all transform bg-white shadow-xl rounded-sm ">
+          <div className="flex flex-col items-center">
+            <Dialog.Title
+              as="h3"
+              className="text-center p-5  flex-auto justify-center text-2xl font-bold py-4"
+            >
+              {t.formatMessage(messages.regionModalHeader)}
+            </Dialog.Title>
+
+            <div className="text-gray-300 py-1 mb-4 flex flex-col items-center w-3/5">
+              <p className="text-sm text-gray-500 self-start mb-1 ">
+                {t.formatMessage(messages.channelFieldLabel)}
+              </p>
+              <select
+                className="w-full text-black"
+                id="channel"
+                {...register("channel", {
+                  required: true,
+                })}
+              >
+                {channels.map(({ slug, name }) => (
+                  <option key={slug} value={slug}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="text-gray-300 py-1 pb-3 flex flex-col items-center w-3/5">
+              <p className="text-sm text-gray-500 self-start mb-1">
+                {t.formatMessage(messages.languageFieldLabel)}
+              </p>
+              <select
+                className="w-full text-black"
+                id="locale"
+                {...register("locale", {
+                  required: true,
+                })}
+              >
+                {LOCALES.map((locale) => (
+                  <option key={locale.slug} value={locale.slug}>
+                    {locale.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="p-3  mt-4 text-center space-x-4 md:block">
+              <Button
+                className="bg-transparent hover:bg-gray-100 text-black py-2 px-4 border border-gray-200 hover:border-gray-500 "
+                onClick={onClose}
+              >
+                Close
+              </Button>
+              <Button onClick={onSubmit}>{t.formatMessage(messages.saveButton)}</Button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </Dialog>
   );
 }
 
