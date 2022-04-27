050eca269847f772fc0b786e4b28682c8c00e942
Adrian Potępa <adrian.potepa@gmail.com>
2022-04-27 14:21:43 +0200

    Added burger menu for smartphones and tablets

---
+++ apps/storefront/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,89 @@
+import { useApolloClient } from "@apollo/client";
+import { useAuth } from "@saleor/sdk";
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useIntl } from "react-intl";
+
+import { usePaths } from "@/lib/paths";
+import { useCheckout } from "@/lib/providers/CheckoutProvider";
+import { useMainMenuQuery } from "@/saleor/api";
+
+import NavIconButton from "../Navbar/NavIconButton";
+import { useRegions } from "../RegionsProvider";
+import { messages } from "../translations";
+import styles from "./BurgerMenu.module.css";
+import CollapseMenu from "./CollapseMenu";
+
+export interface BurgerMenuProps {
+  open?: boolean;
+  onCloseClick?: () => void;
+}
+
+export function BurgerMenu({ open, onCloseClick }: BurgerMenuProps) {
+  const paths = usePaths();
+  const { query } = useRegions();
+  const t = useIntl();
+
+  const { logout } = useAuth();
+  const { resetCheckoutToken } = useCheckout();
+  const router = useRouter();
+  const client = useApolloClient();
+
+  const { error, data } = useMainMenuQuery({
+    variables: { ...query },
+  });
+
+  if (error) {
+    console.error("BurgerMenu component error", error.message);
+  }
+
+  const onLogout = async () => {
+    // clear all the user data on logout
+    await logout();
+    await resetCheckoutToken();
+    await client.resetStore();
+    router.push(paths.$url());
+  };
+
+  const menu = data?.menu?.items || [];
+
+  return (
+    <div
+      className={clsx(styles.container, {
+        [styles["container--open"]]: open,
+      })}
+    >
+      <div className={styles.backdrop} aria-hidden="true" onClick={onCloseClick} />
+      <div className={styles.body}>
+        <div className="flex justify-end w-full mb-5">
+          <NavIconButton icon="close" onClick={onCloseClick} />
+        </div>
+        {menu.map((item) => {
+          if (!item) return null;
+
+          return <CollapseMenu data={item} key={item.id} />;
+        })}
+        <div className="mt-auto pt-4">
+          <div className="flex flex-col">
+            <Link href={paths.account.preferences.$url()} passHref>
+              <a tabIndex={0} className={styles["burger-link"]} href="pass">
+                {t.formatMessage(messages.menuAccountPreferences)}
+              </a>
+            </Link>
+            <button
+              type="button"
+              onClick={onLogout}
+              tabIndex={-1}
+              className={styles["burger-link"]}
+            >
+              {t.formatMessage(messages.logOut)}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BurgerMenu;
--- apps/storefront/components/Navbar/NavIconButton.tsx
+++ apps/storefront/components/Navbar/NavIconButton.tsx
@@ -1,13 +1,14 @@
 import { ButtonHTMLAttributes } from "react";
 
-import Bag from "./bagIcon.svg";
+import Bag from "./bagIcon.svg"; // in the final version it should be imported from ui-kit package
+import Close from "./closeIocn.svg"; // in the final version it should be imported from ui-kit package
 import MenuIcon from "./menuIcon.svg"; // in the final version it should be imported from ui-kit package
 import styles from "./Navbar.module.css"; // in the final version it should be imported from ui-kit package
 import Spyglass from "./spyglassIcon.svg"; // in the final version it should be imported from ui-kit package
 import User from "./userIcon.svg"; // in the final version it should be imported from ui-kit package
 
 interface NavIconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  icon: "user" | "bag" | "spyglass" | "menu";
+  icon: "user" | "bag" | "spyglass" | "menu" | "close";
   counter?: number;
 }
 
@@ -21,6 +22,8 @@ const switchIcon = (icon: NavIconButtonProps["icon"]) => {
       return <Spyglass />;
     case "menu":
       return <MenuIcon />;
+    case "close":
+      return <Close />;
     default:
       return icon;
   }
