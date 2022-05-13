1995f27190f1b892eee9813f912f3de1451ecf57
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-05-13 16:29:31 +0200

    Separate RichText component logic

--- apps/storefront/components/RichText/RichText.tsx
+++ apps/storefront/components/RichText/RichText.tsx
@@ -1,32 +1,16 @@
 import Blocks from "editorjs-blocks-react-renderer";
 import React from "react";
 
+import { parseEditorJSData } from "@/lib/util";
+
 export interface RichTextProps {
   jsonStringData?: string;
 }
 
 export function RichText({ jsonStringData }: RichTextProps) {
-  if (!jsonStringData) {
+  const data = parseEditorJSData(jsonStringData);
+  if (!data) {
     return null;
-  }
-  let data;
-  try {
-    data = JSON.parse(jsonStringData);
-  } catch (e) {
-    return null;
-  }
-
-  if (!data.blocks?.length) {
-    // No data to render
-    return null;
-  }
-
-  // Path for compatibility with data from older version od EditorJS
-  if (!data.time) {
-    data.time = Date.now().toString();
-  }
-  if (!data.version) {
-    data.version = "2.22.2";
   }
 
   return (
