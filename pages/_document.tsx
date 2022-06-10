1b61fb6373e627eb1383612f0df2e54d75494741
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-06-10 10:26:19 +0200

    Redesigned product cards (#248)
    * New product card style
    
    * Use next.js fonts optimization
    
    * Adjust font formatting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,10 @@ class MyDocument extends Document<{ lang?: string }> {
         <Head>
           <link rel="preconnect" href={`//${hostname}`} crossOrigin="true" />
           <link rel="dns-prefetch" href={`//${hostname}`} />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;500;600;800&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
