09cb9f5b17a21abb388fcbc120c83f9386cadc41
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-07-26 20:50:42 +0200

    Update payment providers section styles

--- packages/checkout-storefront/src/components/SelectBoxGroup.tsx
+++ packages/checkout-storefront/src/components/SelectBoxGroup.tsx
@@ -1,14 +1,16 @@
+import clsx from "clsx";
 import { PropsWithChildren } from "react";
+import { Classes } from "../lib/globalTypes";
 
-interface SelectBoxGroupProps {
+interface SelectBoxGroupProps extends Classes {
   label: string;
 }
 
 export const SelectBoxGroup: React.FC<
   PropsWithChildren<SelectBoxGroupProps>
-> = ({ label, children }) => {
+> = ({ label, children, className }) => {
   return (
-    <div role="radiogroup" aria-label={label}>
+    <div role="radiogroup" aria-label={label} className={clsx(className)}>
       {children}
     </div>
   );
--- packages/checkout-storefront/src/components/Title.tsx
+++ packages/checkout-storefront/src/components/Title.tsx
@@ -7,7 +7,7 @@ export const Title: React.FC<PropsWithChildren<Classes>> = ({
   className,
   children,
 }) => (
-  <Text className={clsx(className)} weight="bold">
+  <Text className={clsx("mb-2", className)} weight="bold">
     {children}
   </Text>
 );
