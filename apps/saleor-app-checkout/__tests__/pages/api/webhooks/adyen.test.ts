dd778200f64afb391babffd4fdf4e263caa67d69
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-07-28 17:12:35 +0200

    Add test cases for refund and capture

--- apps/saleor-app-checkout/__tests__/pages/api/webhooks/adyen.test.ts
+++ apps/saleor-app-checkout/__tests__/pages/api/webhooks/adyen.test.ts
@@ -1,6 +1,10 @@
 import { encodeBasicAuth } from "@/saleor-app-checkout/backend/payments/providers/adyen/utils";
 import { testingVars } from "@/saleor-app-checkout/mocks/consts";
-import { getPaymentRequest } from "@/saleor-app-checkout/mocks/fixtures/adyen";
+import {
+  getPaymentCapture,
+  getPaymentRefund,
+  getPaymentRequest,
+} from "@/saleor-app-checkout/mocks/fixtures/adyen";
 import handler from "@/saleor-app-checkout/pages/api/webhooks/adyen";
 import {
   mockRequest,
@@ -72,7 +76,7 @@ describe("/api/webhooks/adyen", () => {
     expect(res.statusCode).toBe(Response.Unauthorized().status);
   });
 
-  it("updates payment when it was completed", async () => {
+  it("updates payment when it was authorized", async () => {
     const { req, res } = mockRequest("POST");
 
     req.headers = getReqHeaders();
@@ -81,5 +85,33 @@ describe("/api/webhooks/adyen", () => {
     await handler(req, res);
 
     expect(res.statusCode).toBe(200);
+
+    // TODO: Check that transaction event "AUTHORIZATION" was created in Saleor + amounts
+  });
+
+  it("updates payment when it was captured", async () => {
+    const { req, res } = mockRequest("POST");
+
+    req.headers = getReqHeaders();
+    req.body = getPaymentCapture();
+
+    await handler(req, res);
+
+    expect(res.statusCode).toBe(200);
+
+    // TODO: Check that transaction event "CAPTURE" was created in Saleor + amounts
+  });
+
+  it("adds refund to Saleor when status changes to REFUND", async () => {
+    const { req, res } = mockRequest("POST");
+
+    req.headers = getReqHeaders();
+    req.body = getPaymentRefund();
+
+    await handler(req, res);
+
+    expect(res.statusCode).toBe(200);
+
+    // TODO: Check that transaction event "REFUND" was created in Saleor + amounts are OK
   });
 });
--- apps/saleor-app-checkout/mocks/fixtures/adyen.ts
+++ apps/saleor-app-checkout/mocks/fixtures/adyen.ts
@@ -1,70 +1,118 @@
 import { adyenHmacValidator } from "@/saleor-app-checkout/backend/payments/providers/adyen/validator";
 import { Types } from "@adyen/api-library";
+import { NotificationRequestItem as AdyenNotificationRequestItem } from "@adyen/api-library/lib/src/typings/notification/notificationRequestItem";
+import { AdditionalData } from "@adyen/api-library/lib/src/typings/notification/additionalData";
 import { testingVars } from "../consts";
 
-export const getPaymentRequest = (hmac?: string | null) => {
-  const notificationItem = {
-    additionalData: {
-      authCode: "085117",
-      avsResult: "4 AVS not supported for this card type",
-      paymentLinkId: "PLEBC2E2F868C9BE80",
-      cardSummary: "0004",
-      authorisationMid: "50",
-      "checkout.cardAddedBrand": "mc",
-      hmacSignature: undefined as string | undefined,
-      // hmacSignature: "qOB9lYiUMCMsJKzCOBQYu3f8pO5FZCd1KTcg+WWivvk=" as
-      //   | undefined
-      //   | string,
-      acquirerAccountCode: "TestPmmAcquirerAccount",
-      cvcResult: "1 Matches",
-      expiryDate: "03/2030",
-      "threeds2.cardEnrolled": "false",
-      "metadata.orderId":
-        "T3JkZXI6MGQ4NDRiZDMtYTA5YS00NzUyLWE0ODktYzFlMzM2Y2I4ZjU4",
-    },
-    amount: { currency: "USD", value: 4292 },
-    eventCode: "AUTHORISATION",
-    eventDate: "2022-07-28T14:08:36+02:00",
-    merchantAccountCode: "SaleorECOM",
-    merchantReference: "3394",
-    operations: ["CANCEL", "CAPTURE", "REFUND"],
-    paymentMethod: "mc",
-    pspReference: "LD65H2FVNXSKGK82",
-    reason: "085117:0004:03/2030",
-    success: "true",
-  } as unknown as Types.notification.NotificationRequestItem;
+declare class NotificationRequestItem extends AdyenNotificationRequestItem {
+  "additionalData": AdditionalData & {
+    [key: string]: string;
+  };
+}
 
-  const getResponse = (item: Types.notification.NotificationRequestItem) => ({
-    live: "false",
-    notificationItems: [
-      {
-        NotificationRequestItem: item,
-      },
-    ],
-  });
+const getNotificationWithHmac =
+  (notificationItem: NotificationRequestItem) => (hmac?: string | null) => {
+    const getResponse = (item: NotificationRequestItem) => ({
+      live: "false",
+      notificationItems: [
+        {
+          NotificationRequestItem: item,
+        },
+      ],
+    });
 
-  if (hmac === null) {
-    return getResponse(notificationItem);
-  }
+    if (hmac === null) {
+      return getResponse(notificationItem);
+    }
 
-  if (typeof hmac === "string") {
+    if (typeof hmac === "string") {
+      return getResponse({
+        ...notificationItem,
+        additionalData: {
+          ...notificationItem.additionalData,
+          hmacSignature: hmac,
+        },
+      });
+    }
+
     return getResponse({
       ...notificationItem,
       additionalData: {
         ...notificationItem.additionalData,
-        hmacSignature: hmac,
+        hmacSignature: adyenHmacValidator.calculateHmac(
+          notificationItem,
+          testingVars.adyenHmac
+        ),
       },
     });
-  }
+  };
 
-  return getResponse({
-    ...notificationItem,
-    additionalData: {
-      ...notificationItem.additionalData,
-      hmacSignature: adyenHmacValidator.calculateHmac(
-        notificationItem,
-        testingVars.adyenHmac
-      ),
-    },
-  });
-};
+export const getPaymentRequest = getNotificationWithHmac({
+  additionalData: {
+    authCode: "085117",
+    avsResult: "4 AVS not supported for this card type",
+    paymentLinkId: "PLEBC2E2F868C9BE80",
+    cardSummary: "0004",
+    authorisationMid: "50",
+    "checkout.cardAddedBrand": "mc",
+    acquirerAccountCode: "TestPmmAcquirerAccount",
+    cvcResult: "1 Matches",
+    expiryDate: "03/2030",
+    "threeds2.cardEnrolled": "false",
+    "metadata.orderId":
+      "T3JkZXI6MGQ4NDRiZDMtYTA5YS00NzUyLWE0ODktYzFlMzM2Y2I4ZjU4",
+  },
+  amount: { currency: "USD", value: 4292 },
+  eventCode: NotificationRequestItem.EventCodeEnum.Authorisation,
+  eventDate: "2022-07-28T14:08:36+02:00",
+  merchantAccountCode: "SaleorECOM",
+  merchantReference: "3394",
+  operations: [
+    "CANCEL",
+    "CAPTURE",
+    "REFUND",
+  ] as unknown as Array<Types.notification.NotificationRequestItem.OperationsEnum>,
+  paymentMethod: "mc",
+  pspReference: "LD65H2FVNXSKGK82",
+  reason: "085117:0004:03/2030",
+  success: NotificationRequestItem.SuccessEnum.True,
+});
+
+export const getPaymentCapture = getNotificationWithHmac({
+  additionalData: {},
+  amount: {
+    currency: "USD",
+    value: 4292,
+  },
+  eventCode: AdyenNotificationRequestItem.EventCodeEnum.Capture,
+  eventDate: "2022-07-28T16:17:01+02:00",
+  merchantAccountCode: "SaleorECOM",
+  merchantReference: "3394",
+  originalReference: "LD65H2FVNXSKGK82",
+  paymentMethod: "mc",
+  pspReference: "NP5DFGQGJRWZNN82",
+  reason: "",
+  success: NotificationRequestItem.SuccessEnum.True,
+});
+
+export const getPaymentRefund = getNotificationWithHmac({
+  additionalData: {
+    bookingDate: "2022-07-28T16:17:47Z",
+    paymentLinkId: "PLEBC2E2F868C9BE80",
+    "metadata.orderId":
+      "T3JkZXI6MGQ4NDRiZDMtYTA5YS00NzUyLWE0ODktYzFlMzM2Y2I4ZjU4",
+  },
+  amount: {
+    currency: "USD",
+    value: 4292,
+  },
+  eventCode: NotificationRequestItem.EventCodeEnum.Refund,
+  eventDate: "2022-07-28T16:17:01+02:00",
+  merchantAccountCode: "SaleorECOM",
+  merchantReference: "3394",
+  originalReference: "LD65H2FVNXSKGK82",
+  paymentMethod: "mc",
+  pspReference: "NP5DFGQGJRWZNN82",
+  reason: "Item returned",
+  success: NotificationRequestItem.SuccessEnum.True,
+});
