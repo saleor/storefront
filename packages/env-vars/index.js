2f74419270924bde49d067478c2922e31237b392
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-07-29 21:30:46 +0200

    Fix env vars loading in Next.js (#320)
    * Fix envs not loading in constants
    
    * Update env-vars to properly load envs from root of monorepo
    
    * Load env-vars package before running Next.js
    
    * Add docs about using env-vars package
    
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- packages/env-vars/index.js
+++ packages/env-vars/index.js
@@ -5,26 +5,52 @@ const fs = require("fs");
 
 // Note - env variables don't override each other
 // if they have been loaded, so the order of loading is flipped from
-// default next.js behavior (first .env.loca, then .env)
+// default next.js behavior (first .env.local, then .env.development, then .env)
 
 // Load env variables from apps/**/.env.local
-let localEnv;
+let localEnv = {};
 if (fs.existsSync(".env.local")) {
   localEnv = dotenv.config({ path: ".env.local" });
+}
+
+// Load env variables from apps/**/.env.developmen
+let developmentEnv = {};
+if (fs.existsSync(".env.development") && process.env.NODE_ENV === "development") {
+  developmentEnv = dotenv.config({path: ".env.development"})
 }
 
 // Load env variables from apps/**/.env
 const env = dotenv.config();
 
 // Load env variables from root of monorepo
-const rootEnvLocal = findConfig(".env.local", { cwd: ".." });
-if (fs.existsSync(rootEnvLocal)) {
-  dotenv.config({ path: rootEnvLocal });
+const rootEnvLocalPath = findConfig(".env.local", { cwd: ".." });
+let rootEnvLocal;
+if (fs.existsSync(rootEnvLocalPath)) {
+  rootEnvLocal = dotenv.config({ path: rootEnvLocalPath });
 }
-dotenv.config({ path: findConfig(".env", { cwd: ".." }) });
+const rootEnv = dotenv.config({ path: findConfig(".env", { cwd: ".." }) });
 
-// Replace $ in .env and .env.local with loaded env variables from root of monorepo
-if (localEnv) {
-  dotenvExpand.expand(localEnv);
+// Merge all envs into single object - see the note about order from top of the file
+const envs = {
+  // project envs
+  ...localEnv?.parsed,
+  ...developmentEnv?.parsed,
+  ...env?.parsed,
+
+  // monorepo envs
+  ...rootEnvLocal?.parsed,
+  ...rootEnv?.parsed,
 }
-dotenvExpand.expand(env);
+
+// Replace $ in variable values with other loaded env variables
+// For example:
+// .env
+//   MY_ENV=123
+// .env.local
+//   OTHER_ENV=$MY_ENV
+// means that OTHER_ENV=123
+const expandedEnvs = dotenvExpand.expand({
+  parsed: envs
+})
+
+module.exports = expandedEnvs
