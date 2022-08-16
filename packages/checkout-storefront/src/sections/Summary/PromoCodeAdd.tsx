edd1a0500db9860c7c3b0407d9fe8772793ccb3f
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-08-16 09:54:24 +0200

    Fix summary items scroll

--- packages/checkout-storefront/src/sections/Summary/PromoCodeAdd.tsx
+++ packages/checkout-storefront/src/sections/Summary/PromoCodeAdd.tsx
@@ -7,8 +7,10 @@ import { useErrors } from "@/checkout-storefront/hooks/useErrors";
 import { useFormattedMessages } from "@/checkout-storefront/hooks/useFormattedMessages";
 import { useGetInputProps } from "@/checkout-storefront/hooks/useGetInputProps";
 import { useSetFormErrors } from "@/checkout-storefront/hooks/useSetFormErrors";
+import { Classes } from "@/checkout-storefront/lib/globalTypes";
 import { extractMutationErrors, useValidationResolver } from "@/checkout-storefront/lib/utils";
-import React from "react";
+import clsx from "clsx";
+import React, { FC } from "react";
 import { useForm } from "react-hook-form";
 import { object, string } from "yup";
 
@@ -16,7 +18,7 @@ interface FormData {
   promoCode: string;
 }
 
-export const PromoCodeAdd = () => {
+export const PromoCodeAdd: FC<Classes> = ({ className }) => {
   const { checkout } = useCheckout();
   const formatMessage = useFormattedMessages();
   const { setApiErrors, errors } = useErrors<FormData>();
@@ -55,7 +57,7 @@ export const PromoCodeAdd = () => {
   });
 
   return (
-    <div className="relative mx-4">
+    <div className={clsx("relative px-4 pt-4", className)}>
       <TextInput label={formatMessage("addDiscount")} {...getInputProps("promoCode")} optional />
       {showApplyButton && (
         <Button
--- packages/checkout-storefront/src/sections/Summary/Summary.tsx
+++ packages/checkout-storefront/src/sections/Summary/Summary.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Text } from "@saleor/ui-kit";
 import { useFormattedMessages } from "@/checkout-storefront/hooks/useFormattedMessages";
 import { SummaryItem } from "./SummaryItem";
@@ -8,7 +8,6 @@ import { ChevronDownIcon } from "@/checkout-storefront/icons";
 import { Transition } from "@headlessui/react";
 import clsx from "clsx";
 import { useCheckout } from "@/checkout-storefront/hooks/useCheckout";
-import { compact } from "lodash-es";
 
 import { getSvgSrc } from "@/checkout-storefront/lib/svgSrc";
 import { Title } from "@/checkout-storefront/components/Title";
@@ -18,20 +17,56 @@ import { SummaryPromoCodeRow } from "./SummaryPromoCodeRow";
 import { SummaryItemMoneyEditableSection } from "./SummaryItemMoneyEditableSection";
 import { getFormattedMoney } from "@/checkout-storefront/lib/utils";
 
+/* temporary solution */
+const PAGE_MARGINS_HEIGHT = 320;
+const LINE_HEIGHT = 104;
+const LG_BREAKPOINT = 1024;
+
 export const Summary = () => {
   const [isOpen, setOpen] = useState(true);
   const { checkout } = useCheckout();
   const { voucherCode, discount } = checkout;
+  const recapRef = useRef<HTMLDivElement>(null);
+  const [maxSummaryHeight, setMaxSummaryHeight] = useState<number>(0);
 
   const formatMessage = useFormattedMessages();
 
   const totalPrice = checkout?.totalPrice?.gross;
   const taxCost = checkout?.totalPrice?.tax;
 
+  const lines = checkout?.lines;
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      const isLg = window.innerWidth > LG_BREAKPOINT;
+
+      if (isLg) {
+        setOpen(true);
+      }
+
+      if (!recapRef.current) {
+        return;
+      }
+
+      const recapHeight = recapRef.current.clientHeight;
+
+      const maxHeight = isLg
+        ? window.innerHeight - PAGE_MARGINS_HEIGHT - recapHeight
+        : lines.length * LINE_HEIGHT;
+
+      setMaxSummaryHeight(maxHeight);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+    handleWindowResize();
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, [recapRef]);
+
   return (
     <div className="summary">
       <div className={clsx("summary-title", isOpen && "open")}>
-        <div className="flex flex-row items-center" onClick={() => setOpen(!isOpen)}>
+        <div className="flex flex-row items-center w-full" onClick={() => setOpen(!isOpen)}>
           <Title className="mb-0">{formatMessage("summary")}</Title>
           <img src={getSvgSrc(ChevronDownIcon)} alt="chevron-down" />
         </div>
@@ -49,15 +84,22 @@ export const Summary = () => {
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
       >
-        <ul className="summary-items">
-          {compact(checkout?.lines)?.map((line) => (
+        <ul
+          className="summary-items"
+          style={{ maxHeight: maxSummaryHeight ? `${maxSummaryHeight}px` : "" }}
+        >
+          {lines.map((line) => (
             <SummaryItem line={line} key={line?.id}>
               <SummaryItemMoneyEditableSection line={line} />
             </SummaryItem>
           ))}
         </ul>
-        <PromoCodeAdd />
-        <div className="summary-recap">
+        <PromoCodeAdd
+          className={clsx(
+            lines.length * LINE_HEIGHT > maxSummaryHeight ? "border-t border-border-secondary" : ""
+          )}
+        />
+        <div className="summary-recap" ref={recapRef}>
           <Divider className="mt-1 mb-4" />
           <SummaryMoneyRow
             label={formatMessage("subtotal")}
