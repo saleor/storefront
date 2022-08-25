d8be74684430f762f88f1826d78393f096a10d96
Michal Miszczyszyn <michal@mmiszy.pl>
2022-08-25 12:17:43 +0200

    DEMO FIX: Use dummy email

--- apps/saleor-app-checkout/backend/payments/createOrder.ts
+++ apps/saleor-app-checkout/backend/payments/createOrder.ts
@@ -64,5 +64,14 @@ export const createOrder = async (
     };
   }
 
+  if (process.env.DEMO_MODE) {
+    return {
+      data: {
+        ...data.orderCreateFromCheckout.order,
+        userEmail: "checkout@example.com",
+      },
+    };
+  }
+
   return { data: data.orderCreateFromCheckout.order };
 };
