c16757524174b3bc9df42488bbf437cd807d4c37
Michal Miszczyszyn <michal@mmiszy.pl>
2022-08-25 17:54:08 +0200

    Add webhooks to manifest

--- apps/saleor-app-checkout/pages/api/manifest.ts
+++ apps/saleor-app-checkout/pages/api/manifest.ts
@@ -1,12 +1,19 @@
+import { print } from "graphql/language/printer.js";
 import { appName } from "../../constants";
 import { version } from "../../package.json";
 import type { Handler } from "retes";
 import { Response } from "retes/response";
 import { toNextHandler } from "retes/adapter";
 import { withBaseURL } from "@saleor/app-sdk/middleware";
+import { getBaseUrl } from "@/saleor-app-checkout/backend/utils";
+import urlJoin from "url-join";
+import { SALEOR_WEBHOOK_TRANSACTION_ENDPOINT } from "./webhooks/saleor/transaction-action-request";
+import { TransactionActionRequestSubscriptionDocument } from "@/saleor-app-checkout/graphql";
 
 const handler: Handler = (request) => {
   const { baseURL } = request.context;
+
+  const webhookUrl = urlJoin(getBaseUrl(request), SALEOR_WEBHOOK_TRANSACTION_ENDPOINT);
 
   const manifest = {
     id: "saleor.checkout.app",
@@ -19,6 +26,15 @@ const handler: Handler = (request) => {
     dataPrivacyUrl: `${baseURL}/data-privacy`,
     supportUrl: `${baseURL}/support`,
     tokenTargetUrl: `${baseURL}/api/register`,
+    webhooks: [
+      {
+        name: "Checkout app payment notifications",
+        events: ["TRANSACTION_ACTION_REQUEST"],
+        query: print(TransactionActionRequestSubscriptionDocument),
+        targetUrl: webhookUrl,
+        isActive: true,
+      },
+    ],
   };
 
   return Response.OK(manifest);
