d7eaf4380e2692b08e7938212ae1a80d6b8bdce7
Michal Miszczyszyn <michal@mmiszy.pl>
2022-08-30 17:49:41 +0200

    Remove testing code

--- apps/saleor-app-checkout/pages/api/test-sentry.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { withSentry } from "@sentry/nextjs";
-
-const handler = async (_req: NextApiRequest, _res: NextApiResponse) => {
-  throw new Error("API throw error test");
-};
-
-export default withSentry(handler);
