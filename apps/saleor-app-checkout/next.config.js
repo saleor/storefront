2b85af5b4bad0309497deecc4f3adb2e0da758f1
Michal Miszczyszyn <michal@mmiszy.pl>
2022-08-30 18:26:22 +0200

    Fix build

--- apps/saleor-app-checkout/next.config.js
+++ apps/saleor-app-checkout/next.config.js
@@ -1,42 +1,46 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 const withTM = require("next-transpile-modules")();
 
-/** @type {import('next').NextConfig} */
-module.exports = withSentryConfig(
-  withTM({
-    i18n: {
-      locales: ["en-US", "pl-PL", "fr-FR", "vi-VN"],
-      defaultLocale: "en-US",
-    },
-    reactStrictMode: true,
-    // eslint-disable-next-line require-await
-    async redirects() {
-      return [
-        {
-          source: "/",
-          destination: "/channels",
-          permanent: true,
-        },
-      ];
-    },
-    images: { domains: ["localhost"] },
-    experimental: {
-      esmExternals: false,
-    },
-    eslint: {
-      dirs: ["pages", "backend", "frontend", "config"],
-    },
-    assetPrefix: process.env.NEXT_PUBLIC_CHECKOUT_APP_URL,
-  }),
-  {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+const isSentryEnabled =
+  process.env.SENTRY_DSN && process.env.SENTRY_ENVIRONMENT && process.env.SENTRY_RELEASE;
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-  }
-);
+/** @type {import('next').NextConfig} */
+const config = withTM({
+  i18n: {
+    locales: ["en-US", "pl-PL", "fr-FR", "vi-VN"],
+    defaultLocale: "en-US",
+  },
+  reactStrictMode: true,
+  // eslint-disable-next-line require-await
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/channels",
+        permanent: true,
+      },
+    ];
+  },
+  images: { domains: ["localhost"] },
+  experimental: {
+    esmExternals: false,
+  },
+  eslint: {
+    dirs: ["pages", "backend", "frontend", "config"],
+  },
+  assetPrefix: process.env.NEXT_PUBLIC_CHECKOUT_APP_URL,
+});
+
+module.exports = isSentryEnabled
+  ? withSentryConfig(config, {
+      // Additional config options for the Sentry Webpack plugin. Keep in mind that
+      // the following options are set automatically, and overriding them is not
+      // recommended:
+      //   release, url, org, project, authToken, configFile, stripPrefix,
+      //   urlPrefix, include, ignore
+
+      silent: true, // Suppresses all logs
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options.
+    })
+  : config;
