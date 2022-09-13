802c5a004c6232cfcd808340c8010c29244ff0d3
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-09-13 13:20:28 +0200

    Fix select stories

---
+++ packages/ui-kit/src/lib/utils.ts
@@ -0,0 +1,13 @@
+import { SyntheticEvent, useState } from "react";
+
+export const useStateWithOnChangeHandler = <TValue extends string | boolean = string>(
+  initialValue: TValue = "" as TValue
+): [TValue, (event: SyntheticEvent) => void] => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (event: SyntheticEvent) => {
+    setValue((event.target as HTMLInputElement).value as TValue);
+  };
+
+  return [value, handleChange];
+};
