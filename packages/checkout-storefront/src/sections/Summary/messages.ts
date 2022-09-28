34fbbe8fd2683e77ab37c71628ec26e3fe6f184a
Michal Miszczyszyn <michal@mmiszy.pl>
2022-09-28 12:48:45 +0200

    Add missing cli

--- packages/checkout-storefront/src/sections/Summary/messages.ts
+++ packages/checkout-storefront/src/sections/Summary/messages.ts
@@ -1,5 +1,5 @@
 import { defineMessages } from "react-intl";
 
 export const summaryMessages = defineMessages({
-  title: { defaultMessage: "Summary", description: "summary title" },
+  title: { defaultMessage: "Summary", id: "Summary/messages/62kM+I", description: "summary title" },
 });
