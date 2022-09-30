3279d0c99146b8e990feaecae9096e1cd4bb39fe
Michał Miszczyszyn <michal@mmiszy.pl>
2022-09-30 15:46:28 +0200

    497: Disable spellchecker on PII and password fields (#498)
    Fixes #497
    Fixes https://app.clickup.com/t/2549495/SALEOR-8085
    Fixes https://app.clickup.com/t/2549495/SALEOR-8084

--- apps/storefront/pages/[channel]/[locale]/account/login.tsx
+++ apps/storefront/pages/[channel]/[locale]/account/login.tsx
@@ -81,6 +81,7 @@ function LoginPage() {
                   className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none"
                   type="email"
                   id="email"
+                  spellCheck={false}
                   {...registerForm("email", {
                     required: true,
                   })}
@@ -94,6 +95,7 @@ function LoginPage() {
                   className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none"
                   type="password"
                   id="password"
+                  spellCheck={false}
                   {...registerForm("password", {
                     required: true,
                   })}
