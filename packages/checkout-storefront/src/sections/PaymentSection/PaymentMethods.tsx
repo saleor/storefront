2fffc3dcf40eaee60f2ea0dd9114a15c0ab61dfb
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-10-03 10:55:31 +0200

    Add more messages

--- packages/checkout-storefront/src/sections/PaymentSection/PaymentMethods.tsx
+++ packages/checkout-storefront/src/sections/PaymentSection/PaymentMethods.tsx
@@ -1,11 +1,11 @@
-import { MessageKey, useFormattedMessages } from "@/checkout-storefront/hooks/useFormattedMessages";
+import { useFormattedMessages } from "@/checkout-storefront/hooks/useFormattedMessages";
 import React from "react";
-import { camelCase } from "lodash-es";
 import { PaymentMethodID } from "checkout-common";
 import { SelectBoxGroup } from "@/checkout-storefront/components/SelectBoxGroup";
 import { SelectBox } from "@/checkout-storefront/components/SelectBox";
 import { Text } from "@saleor/ui-kit";
 import { UsePaymentMethods } from "./usePaymentMethods";
+import { paymentMethodsMessages, labels } from "./messages";
 
 export type PaymentMethodsProps = UsePaymentMethods;
 
@@ -17,7 +17,7 @@ export const PaymentMethods: React.FC<PaymentMethodsProps> = ({
   const formatMessage = useFormattedMessages();
 
   return (
-    <SelectBoxGroup label={formatMessage("paymentProvidersLabel")} className="flex flex-row gap-2">
+    <SelectBoxGroup label={formatMessage(labels.paymentProviders)} className="flex flex-row gap-2">
       {availablePaymentMethods.map((paymentMethodId: PaymentMethodID) => (
         <SelectBox
           className="shrink"
@@ -25,7 +25,7 @@ export const PaymentMethods: React.FC<PaymentMethodsProps> = ({
           selectedValue={selectedPaymentMethod}
           onSelect={(value: string) => setSelectedPaymentMethod(value as PaymentMethodID)}
         >
-          <Text>{formatMessage(camelCase(paymentMethodId) as MessageKey)}</Text>
+          <Text>{formatMessage(paymentMethodsMessages[paymentMethodId])}</Text>
         </SelectBox>
       ))}
     </SelectBoxGroup>
