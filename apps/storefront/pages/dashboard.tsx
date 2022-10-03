df3fb85c1cf71fc4eeeb7d8fda7a587ac3d6d5e8
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-10-03 21:36:28 +0200

    better dashboard page

--- apps/storefront/pages/dashboard.tsx
+++ apps/storefront/pages/dashboard.tsx
@@ -1,4 +1,6 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export const isInIframe = () => {
   try {
@@ -8,12 +10,43 @@ export const isInIframe = () => {
   }
 };
 
+const useIsMounted = (): boolean => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  return mounted;
+};
+
+const getStorefrontUrl = () => window.location.origin;
+
 const DashboardPage: NextPage = () => {
-  if (!isInIframe()) {
-    return <div>This is forbidden page</div>;
+  const mounted = useIsMounted();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (mounted && !isInIframe()) {
+      void push("/");
+    }
+  }, [mounted, push]);
+
+  if (!mounted || !isInIframe()) {
+    return null;
   }
 
-  return <div>This page is visible in dashboard</div>;
+  return (
+    <div>
+      <h1>Saleor Storefront example</h1>
+      <a href={getStorefrontUrl()} target="_blank" rel="noreferrer">
+        Access storefront
+      </a>
+      <div>
+        <code>
+          <pre>CHECKOUT_STOREFRONT_URL={process.env.CHECKOUT_STOREFRONT_URL}</pre>
+        </code>
+      </div>
+      <p>todo: links to docs</p>
+    </div>
+  );
 };
 
 export default DashboardPage;
