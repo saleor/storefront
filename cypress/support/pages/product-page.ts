23d738c1c7c589f48a89c16e7c28f76ea0faa733
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2022-10-06 10:53:55 +0200

    Fix add variant to cart test after removing cart view (#512)
    Fix test and remove cypress from dependencies in app/storefront

--- cypress/support/pages/product-page.ts
+++ cypress/support/pages/product-page.ts
@@ -12,47 +12,21 @@ export function openProductPage(product) {
     .should("be.visible");
 }
 
-export function addItemToCart(selectedItem) {
-  cy.get(PRODUCT_ELEMENTS.addToCartButton).then((addToCartButton) => {
-    if (addToCartButton.is(":disabled")) {
-      cy.get(PRODUCT_ELEMENTS.soldOut)
-        .should("not.exist")
-        .get(selectedItem)
-        .invoke("text")
-        .then((selectedProductName) => {
-          cy.get(PRODUCT_ELEMENTS.variant)
-            .first()
-            .invoke("text")
-            .then((selectedVariantName) => {
-              cy.get(PRODUCT_ELEMENTS.variant)
-                .first()
-                .click()
-                .get(PRODUCT_ELEMENTS.addToCartButton)
-                .click()
-                .url()
-                .should("include", "/cart")
-                .get(CART.cartVariant)
-                .first()
-                .should("contain.text", selectedVariantName);
-            })
-            .get(CART.cartProduct)
-            .first()
-            .should("contain.text", selectedProductName);
-        });
-    } else {
-      cy.get(PRODUCT_ELEMENTS.soldOut)
-        .should("not.exist")
-        .get(selectedItem)
-        .invoke("text")
-        .then((selectedProductName) => {
-          cy.get(PRODUCT_ELEMENTS.addToCartButton)
-            .click()
-            .url()
-            .should("include", "/cart")
-            .get(CART.cartProduct)
-            .first()
-            .should("contain.text", selectedProductName);
-        });
-    }
-  });
+export function addItemToCart() {
+  cy.addAliasToGraphRequest("CreateCheckout");
+  cy.get(PRODUCT_ELEMENTS.soldOut)
+    .should("not.exist")
+    .get(PRODUCT_ELEMENTS.addToCartButton)
+    .then((addToCartButton) => {
+      if (addToCartButton.is(":disabled")) {
+        cy.get(PRODUCT_ELEMENTS.variant)
+          .first()
+          .click()
+          .get(PRODUCT_ELEMENTS.addToCartButton)
+          .click();
+      } else {
+        cy.get(PRODUCT_ELEMENTS.addToCartButton).click();
+      }
+      cy.wait("@CreateCheckout").its("response.body.data.checkoutCreate.errors").should("be.empty");
+    });
 }
