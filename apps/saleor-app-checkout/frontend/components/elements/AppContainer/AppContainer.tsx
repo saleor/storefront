78ed9fe539c4841e93d2af127dae13782683ac75
Michal Miszczyszyn <michal@mmiszy.pl>
2022-10-21 16:57:56 +0200

    Fix blue bottom

--- apps/saleor-app-checkout/frontend/components/elements/AppContainer/AppContainer.tsx
+++ apps/saleor-app-checkout/frontend/components/elements/AppContainer/AppContainer.tsx
@@ -1,8 +1,14 @@
+import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 import { useStyles } from "./styles";
 
 const AppContainer: React.FC<{ children: ReactNode }> = (props) => {
   const classes = useStyles();
+  const router = useRouter();
+
+  if (router.pathname === "/checkout-spa") {
+    return <div {...props} />;
+  }
 
   return <div className={classes.root} {...props} />;
 };
