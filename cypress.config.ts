312f8818a285febcd76df1932531ec5c5f2cb781
Anna Szczech <anna.m.szczech@gmail.com>
2022-10-24 16:34:52 +0200

    update workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "cypress";
-
-require("dotenv").config();
+import "../react-storefront/packages/env-vars";
 
 export default defineConfig({
   projectId: "o9u948",
@@ -12,11 +11,11 @@ export default defineConfig({
   requestTimeout: 15000,
   viewportWidth: 1400,
   viewportHeight: 660,
+  env: {
+    API_URL: process.env.SALEOR_API_URL,
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    env: {
-      API_URL: process.env.SALEOR_API_URL,
-    },
   },
 });
