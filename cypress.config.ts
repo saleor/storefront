3cbcd0185d99d085b3a88656e760c25145ed18d3
Anna Szczech <anna.m.szczech@gmail.com>
2022-10-27 08:36:52 +0200

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from "cypress";
+import cypress, { defineConfig } from "cypress";
 import "../react-storefront/packages/env-vars";
 
 export default defineConfig({
@@ -11,23 +11,26 @@ export default defineConfig({
   requestTimeout: 15000,
   viewportWidth: 1400,
   viewportHeight: 660,
+  env: {
+    API_URL: process.env.SALEOR_API_URL,
+    userEmail: "test1@user.com",
+    userPassword: "test1234",
+    digitalProduct: "sea",
+    productWithoutVariants: "Lager",
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    env: {
-      API_URL: process.env.SALEOR_API_URL,
-      userEmail: "test1@user.com",
-      userPassword: "test1234",
-      digitalProduct: "sea",
-      productWithoutVariants: "Lager",
-    },
     setupNodeEvents(on, config) {
-      // console.log(config) // see everything in here!
-      // modify config values
-      config.defaultCommandTimeout = 10000;
-      (config.baseUrl = "https://saleor-storefront-flax.vercel.app/"),
-        // modify env var value
-        (config.env.ENVIRONMENT = "staging");
+      (config.baseUrl = "https://staging-demo.saleor.io/"),
+        (config.env.ENVIRONMENT = "staging"),
+        (config.env.userEmail = "admin@example.com"),
+        (config.env.userPassword = "admin"),
+        (config.env.digitalProduct = "cloud"),
+        (config.env.productWithoutVariants = "beanie"),
+        (config.env.API_URL = "https://staging-demo.saleor.io/graphql/");
+
+      console.log(config.env);
 
       // IMPORTANT return the updated config object
       return config;
