18dc45fa4d7884040dccbd0a7625fc77a0681523
Michal Miszczyszyn <michal@mmiszy.pl>
2023-03-03 21:26:22 +0100

    Revert "Add keeping values and dirty in useForm for autocomplete to work (#757)"
    This reverts commit 28cb9cdcefa32a44df0567e3ef9832f70786ffe1.

--- packages/checkout-storefront/src/hooks/useForm/useForm.ts
+++ packages/checkout-storefront/src/hooks/useForm/useForm.ts
@@ -1,59 +1,28 @@
-import { ChangeHandler, FormDataBase, FormDataField, FormProps, UseFormReturn } from "./types";
+import { FormDataBase, FormProps, UseFormReturn } from "@/checkout-storefront/hooks/useForm/types";
 import { useFormik, useFormikContext } from "formik";
-import { isEqual } from "lodash-es";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 
 export const useForm = <TData extends FormDataBase>({
-  initialDirty = false,
+  initialDirty,
   ...formProps
 }: FormProps<TData>): UseFormReturn<TData> => {
   const form = useFormik<TData>(formProps);
-  // we do this because in some cases it's not updated properly
-  // https://github.com/jaredpalmer/formik/issues/3165
-  const [dirty, setDirty] = useState(initialDirty);
-  const [values, setValues] = useState(formProps.initialValues);
 
-  const {
-    handleSubmit: handleFormikSubmit,
-    handleChange: formikHandleChange,
-    setValues: setFormikValues,
-  } = form;
+  const { dirty, handleSubmit: handleFormikSubmit } = form;
 
   const handleSubmit = useCallback(
     (event?: React.FormEvent<HTMLFormElement>) => {
       event?.preventDefault();
 
       // we do it here because formik doesn't pass props like dirty to onSubmit
-      if (dirty) {
+      if (initialDirty || dirty) {
         handleFormikSubmit(event);
       }
     },
-    [dirty, handleFormikSubmit]
+    [dirty, handleFormikSubmit, initialDirty]
   );
 
-  const handleChange: ChangeHandler = useCallback(
-    (event) => {
-      const { name, value } = event.target;
-
-      const updatedValues = { ...values, [name]: value };
-
-      setDirty(!isEqual(values, updatedValues));
-      setValues(updatedValues);
-      formikHandleChange(event);
-    },
-    [formikHandleChange, values]
-  );
-
-  const setFieldValue = useCallback(
-    <TName extends FormDataField<TData>>(name: TName, value: TData[TName]) => {
-      const updatedValues = { ...values, [name]: value };
-      setValues(updatedValues);
-      void setFormikValues(updatedValues);
-    },
-    [setFormikValues, values]
-  );
-
-  return { ...form, handleSubmit, handleChange, values, dirty, setFieldValue };
+  return { ...form, handleSubmit };
 };
 
 export const useFormContext = useFormikContext;
