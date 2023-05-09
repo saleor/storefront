415342fd841efe71b143da5454733d9f2065d2f8
Michał Miszczyszyn <michal@mmiszy.pl>
2023-05-09 18:13:58 +0200

    Allow only certain api urls (#833)

--- packages/checkout-storefront/src/pages/_app.tsx
+++ packages/checkout-storefront/src/pages/_app.tsx
@@ -20,6 +20,7 @@ const checkoutApiUrl = process.env["NEXT_PUBLIC_CHECKOUT_APP_URL"]
   ? urlJoin(process.env["NEXT_PUBLIC_CHECKOUT_APP_URL"], `api`)
   : "";
 const checkoutAppUrl = process.env["NEXT_PUBLIC_CHECKOUT_APP_URL"];
+const allowedSaleorApiRegex = process.env["NEXT_PUBLIC_ALLOWED_SALEOR_API_REGEX"];
 
 export default function CheckoutSpa() {
   if (!checkoutApiUrl) {
@@ -30,6 +31,15 @@ export default function CheckoutSpa() {
     console.warn(`Missing NEXT_PUBLIC_CHECKOUT_APP_URL env variable`);
     return null;
   }
+  if (!allowedSaleorApiRegex) {
+    console.warn(`Missing NEXT_PUBLIC_ALLOWED_SALEOR_API_REGEX  env variable`);
+    return null;
+  }
 
-  return <CheckoutStoreFront env={{ checkoutApiUrl, checkoutAppUrl }} />;
+  return (
+    <CheckoutStoreFront
+      env={{ checkoutApiUrl, checkoutAppUrl }}
+      saleorApiUrlRegex={new RegExp(allowedSaleorApiRegex)}
+    />
+  );
 }
