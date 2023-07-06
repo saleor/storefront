58a39f90918422b3c01446682eb1e094795e8e3e
IgorSwatowski <igi66@wp.pl>
2023-07-06 12:01:50 +0200

    Delete unused code on homepage. Add rootCategory function and display root categories on the homepage

--- apps/storefront/pages/[channel]/[locale]/index.tsx
+++ apps/storefront/pages/[channel]/[locale]/index.tsx
@@ -1,55 +1,102 @@
-import { ApolloQueryResult } from "@apollo/client";
-import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { GetStaticPaths, InferGetStaticPropsType } from "next";
 import React, { ReactElement } from "react";
+import Image from "next/image";
 
-import { HomepageBlock, Layout } from "@/components";
+import { Layout } from "@/components";
 import { BaseSeo } from "@/components/seo/BaseSeo";
-import { HOMEPAGE_MENU } from "@/lib/const";
-import { contextToRegionQuery } from "@/lib/regions";
-import {
-  HomepageBlocksQuery,
-  HomepageBlocksQueryDocument,
-  HomepageBlocksQueryVariables,
-} from "@/saleor/api";
-import { serverApolloClient } from "@/lib/ssr/common";
+import { rootCategoryPaths } from "@/lib/ssr/category";
 
-export const getStaticProps = async (context: GetStaticPropsContext) => {
-  const result: ApolloQueryResult<HomepageBlocksQuery> = await serverApolloClient.query<
-    HomepageBlocksQuery,
-    HomepageBlocksQueryVariables
-  >({
-    query: HomepageBlocksQueryDocument,
-    variables: { slug: HOMEPAGE_MENU, ...contextToRegionQuery(context) },
-  });
+import WomanCategory from "../../../images/homepage/woman-category.jpg";
+import ManCategory from "../../../images/homepage/man-category.jpg";
+import KidCategory from "../../../images/homepage/kid-category.jpg";
+
+export const WomanCategoryImg = WomanCategory;
+
+export const KidCategoryImg = KidCategory;
+
+export const ManCategoryImg = ManCategory;
+
+export const getStaticProps = async () => {
+  const paths = await rootCategoryPaths();
+
+  const categories = paths.map((path) => ({
+    slug: path.params.slug,
+  }));
+
   return {
     props: {
-      menuData: result?.data,
+      categories,
     },
-    revalidate: 60 * 60, // value in seconds, how often ISR will trigger on the server
+    revalidate: 60 * 60,
   };
 };
-function Home({ menuData }: InferGetStaticPropsType<typeof getStaticProps>) {
+
+type HomeProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Home: React.FC<HomeProps> = ({ categories }) => {
+  const categoriesExist = () => {
+    return categories.length > 0;
+  };
+
   return (
     <>
       <BaseSeo />
       <div className="py-10">
         <header className="mb-4">
-          <div className="container" />
+          <div className="container">
+            {categoriesExist() && (
+              <section className="home-page__categories">
+                <div className="container home-page__categories-wrapper">
+                  <h2 className="home-page__categories-wrapper-title">
+                    Szukasz konkretnych <span>produktów?</span> Nasze kategorie ułatwią Ci zadanie!
+                  </h2>
+                  <p className="home-page__categories-wrapper-subtitle">
+                    Nie wiesz, gdzie szukać swoich ulubionych produktów? Skorzystaj z naszych
+                    przejrzystych kategorii i z łatwością znajdź to, czego szukasz.
+                  </p>
+                </div>
+                <div className="container home-page__categories_container">
+                  <div className="home-page__categories__list">
+                    {categories.map((category) => {
+                      return (
+                        <div key={category.slug} className="home-page__category-item">
+                          <div className="home-page__categories__text-image">
+                            {category.slug === "apparel" ? (
+                              <Image src={WomanCategoryImg} alt="1" />
+                            ) : category.slug === "accessories" ? (
+                              <Image src={ManCategoryImg} alt="1" />
+                            ) : (
+                              category.slug === "default-category" && (
+                                <Image src={KidCategoryImg} alt="1" />
+                              )
+                            )}
+                            <div className="home-page__categories-wrapper">
+                              {/* <Link
+                                href={generatePath(paths.category, {
+                                  slug: category.slug,
+                                })}
+                                key={category.id}
+                              >
+                                <a className="home-page__categories__text-title">{category.name}</a>
+                              </Link> */}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </section>
+            )}
+          </div>
         </header>
         <main>
-          <div className="container">
-            {menuData?.menu?.items?.map((m) => {
-              if (!m) {
-                return null;
-              }
-              return <HomepageBlock key={m.id} menuItem={m} />;
-            })}
-          </div>
+          <div className="container"></div>
         </main>
       </div>
     </>
   );
-}
+};
 
 export default Home;
 
