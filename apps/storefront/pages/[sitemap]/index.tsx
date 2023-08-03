63434daa97baa65c1aaaae2f0ba4aca71df47fed
oleksyremigiusz <oleksyremigiusz@gmail.com>
2023-08-03 11:56:35 +0200

    remove old sitemap, host fix

--- apps/storefront/pages/[sitemap]/index.tsx
+++ apps/storefront/pages/[sitemap]/index.tsx
@@ -16,7 +16,7 @@ function generateSiteMap(data) {
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>http://localhost:3000/</loc>
+      <loc>${process.env.STOREFRONT_URL}/</loc>
     </url>
   ${data.sitemapSlugs.pagesSlugs
     .map((slug) => {
