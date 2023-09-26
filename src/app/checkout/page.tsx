75c95e1408d28ed7d718c651481a8954582f6213
Michal Miszczyszyn <michal@mmiszy.pl>
2023-09-26 18:32:48 +0200

    wip

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,11 +1,13 @@
-export default async function CheckoutPage() {
-  return (
-    <section className="max-w-7xl mx-auto p-8">
-      <h1 className="mt-8 text-3xl font-bold text-gray-900">Checkout</h1>
+import { RootWrapper } from "./pageWrapper";
 
-      <form className="mt-12">
-        <div>Soon</div>
-      </form>
-    </section>
-  );
+export default function CheckoutPage() {
+	return (
+		<section className="mx-auto max-w-7xl p-8">
+			<h1 className="mt-8 text-3xl font-bold text-gray-900">Checkout</h1>
+
+			<form className="mt-12">
+				<RootWrapper saleorApiUrl={process.env.SALEOR_API_URL!} />
+			</form>
+		</section>
+	);
 }
--- src/checkout/src/Root.tsx
+++ src/checkout/src/Root.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ErrorBoundary } from "react-error-boundary";
 import { IntlProvider } from "react-intl";
 import {
@@ -17,18 +18,9 @@ import { alertsContainerProps } from "./hooks/useAlerts/consts";
 import { RootViews } from "./views/RootViews";
 import { useLocale } from "./hooks/useLocale";
 import { DEFAULT_LOCALE } from "./lib/regions";
-import { getQueryParams } from "./lib/utils/url";
 import { PageNotFound } from "@/checkout/src/views/PageNotFound";
-import { type AppEnv } from "@/checkout/src/providers/AppConfigProvider/types";
-import { AppConfigProvider } from "@/checkout/src/providers/AppConfigProvider";
 
-export interface RootProps {
-	env: AppEnv;
-}
-
-export const Root = ({ env }: RootProps) => {
-	const { saleorApiUrl } = getQueryParams();
-
+export const Root = ({ saleorApiUrl }: { saleorApiUrl: string }) => {
 	const saleorAuthClient = useMemo(() => createSaleorAuthClient({ saleorApiUrl }), [saleorApiUrl]);
 
 	const makeUrqlClient = () =>
@@ -63,14 +55,12 @@ export const Root = ({ env }: RootProps) => {
 		<IntlProvider defaultLocale={DEFAULT_LOCALE} locale={locale} messages={messages}>
 			<SaleorAuthProvider client={saleorAuthClient}>
 				<UrqlProvider value={urqlClient}>
-					<AppConfigProvider env={env}>
-						<div className="app">
-							<ToastContainer {...alertsContainerProps} />
-							<ErrorBoundary FallbackComponent={PageNotFound}>
-								<RootViews />
-							</ErrorBoundary>
-						</div>
-					</AppConfigProvider>
+					<div className="app">
+						<ToastContainer {...alertsContainerProps} />
+						<ErrorBoundary FallbackComponent={PageNotFound}>
+							<RootViews />
+						</ErrorBoundary>
+					</div>
 				</UrqlProvider>
 			</SaleorAuthProvider>
 		</IntlProvider>
