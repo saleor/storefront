73606f62cba0698c1f597bae09ae7801d2f000fc
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-05 17:40:23 +0200

    fix: improve a11y of variant selector

--- src/ui/components/VariantSelector.tsx
+++ src/ui/components/VariantSelector.tsx
@@ -2,7 +2,7 @@
 
 import { clsx } from "clsx";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useId } from "react";
 
 export const createPathname = (pathname: string, params: URLSearchParams) => {
 	const paramsString = params.toString();
@@ -39,28 +39,38 @@ export function VariantSelector(props: { variants: { id: string; name: string }[
 		}
 	}, [selectVariant, variants]);
 
+	const generatedId = useId();
+
 	return (
-		<div className="my-4">
-			<div className="sr-only">Variants</div>
+		<div className="my-4" role="radiogroup" aria-labelledby={`${generatedId}-title`}>
+			<div className="sr-only" id={`${generatedId}-title`}>
+				Variants
+			</div>
 			<div className="grid grid-cols-2 gap-3 sm:grid-cols-4">
 				{variants.length > 1 &&
 					variants.map((variant) => {
 						return (
-							<button
+							<label
 								key={variant.id}
-								type="button"
-								onClick={() => {
-									selectVariant(variant.id);
-								}}
+								htmlFor={`${generatedId}-variant-${variant.id}`}
 								className={clsx(
 									searchParams.get("variant") === variant.id
 										? "border-transparent bg-slate-600 text-white hover:bg-slate-500"
 										: "border-slate-200 bg-white text-slate-900 hover:bg-slate-100",
-									"block overflow-hidden text-ellipsis rounded border p-3 text-sm font-semibold",
+									"relative flex items-center justify-center overflow-hidden text-ellipsis rounded border p-3 text-center text-sm font-semibold focus-within:outline focus-within:outline-2",
 								)}
 							>
+								<input
+									type="radio"
+									name="variant"
+									id={`${generatedId}-variant-${variant.id}`}
+									className="sr-only"
+									onClick={() => {
+										selectVariant(variant.id);
+									}}
+								/>
 								{variant.name}
-							</button>
+							</label>
 						);
 					})}
 			</div>
