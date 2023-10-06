d60ba7c05dde9847f28395f5f88b744929d09652
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-06 12:36:35 +0200

    comeback to buttons in VariantSelector

--- src/ui/components/VariantSelector.tsx
+++ src/ui/components/VariantSelector.tsx
@@ -2,7 +2,7 @@
 
 import { clsx } from "clsx";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useEffect, useId } from "react";
+import { useCallback, useEffect } from "react";
 
 export const createPathname = (pathname: string, params: URLSearchParams) => {
 	const paramsString = params.toString();
@@ -39,41 +39,33 @@ export function VariantSelector(props: { variants: { id: string; name: string }[
 		}
 	}, [selectVariant, variants]);
 
-	const generatedId = useId();
-
 	return (
-		<div className="my-4" role="radiogroup" aria-labelledby={`${generatedId}-title`}>
-			<div className="sr-only" id={`${generatedId}-title`}>
-				Variants
-			</div>
+		<fieldset className="my-4" role="radiogroup">
+			<legend className="sr-only">Variants</legend>
 			<div className="grid grid-cols-2 gap-3 sm:grid-cols-4">
 				{variants.length > 1 &&
 					variants.map((variant) => {
 						return (
-							<label
+							<button
 								key={variant.id}
-								htmlFor={`${generatedId}-variant-${variant.id}`}
+								type="button"
+								onClick={() => {
+									selectVariant(variant.id);
+								}}
 								className={clsx(
 									searchParams.get("variant") === variant.id
 										? "border-transparent bg-slate-600 text-white hover:bg-slate-500"
 										: "border-slate-200 bg-white text-slate-900 hover:bg-slate-100",
 									"flex items-center justify-center overflow-hidden text-ellipsis rounded border p-3 text-center text-sm font-semibold focus-within:outline focus-within:outline-2",
 								)}
+								role="radio"
+								aria-checked={searchParams.get("variant") === variant.id}
 							>
-								<input
-									type="radio"
-									name="variant"
-									id={`${generatedId}-variant-${variant.id}`}
-									className="sr-only"
-									onClick={() => {
-										selectVariant(variant.id);
-									}}
-								/>
 								{variant.name}
-							</label>
+							</button>
 						);
 					})}
 			</div>
-		</div>
+		</fieldset>
 	);
 }
