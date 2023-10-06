20166f2e96fa597065c3e3e8c7c04f5ff70804f7
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-06 12:55:58 +0200

    fix typo

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
 				</div>
 			</div>
 			<section className="sm:py-18 mx-auto max-w-2xl px-8 py-12 sm:px-6 lg:max-w-7xl">
-				<h2 className="sr-only">Product in category {category.name}</h2>
+				<h2 className="sr-only">Products in category {category.name}</h2>
 				<div className="mt-4 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
 					{products?.edges.map(({ node: product }) => <ProductElement key={product.id} product={product} />)}
 				</div>
