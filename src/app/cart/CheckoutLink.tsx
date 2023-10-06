3e007526a5e535af2843e271f7489e0f0b4f013e
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-06 11:52:59 +0200

    Update src/app/cart/CheckoutLink.tsx
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- src/app/cart/CheckoutLink.tsx
+++ src/app/cart/CheckoutLink.tsx
@@ -15,7 +15,7 @@ export const CheckoutLink = ({ disabled, checkoutId }: Props) => {
 			onClick={(e) => disabled && e.preventDefault()}
 			href={`/checkout?checkout=${checkoutId}`}
 			className={clsx(
-				disabled && "cursor-not-allowed",
+				"aria-disabled:cursor-not-allowed",
 				"w-full rounded border border-transparent bg-pink-600 px-6 py-3 text-center font-medium text-gray-50 hover:bg-pink-500 aria-disabled:bg-pink-500 sm:col-start-2",
 			)}
 		>
