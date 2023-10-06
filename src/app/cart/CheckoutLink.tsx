de3e022ec8319ac9aa1eb66e16af39b04bd1e47d
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-06 11:54:21 +0200

    refactor: refactor CheckoutLink

--- src/app/cart/CheckoutLink.tsx
+++ src/app/cart/CheckoutLink.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import clsx from "clsx";
 import Link from "next/link";
 
 type Props = {
@@ -14,10 +13,7 @@ export const CheckoutLink = ({ disabled, checkoutId }: Props) => {
 			aria-disabled={disabled}
 			onClick={(e) => disabled && e.preventDefault()}
 			href={`/checkout?checkout=${checkoutId}`}
-			className={clsx(
-				"aria-disabled:cursor-not-allowed",
-				"w-full rounded border border-transparent bg-pink-600 px-6 py-3 text-center font-medium text-gray-50 hover:bg-pink-500 aria-disabled:bg-pink-500 sm:col-start-2",
-			)}
+			className="w-full rounded border border-transparent bg-pink-600 px-6 py-3 text-center font-medium text-gray-50 hover:bg-pink-500 aria-disabled:cursor-not-allowed aria-disabled:bg-pink-500 sm:col-start-2"
 		>
 			Checkout
 		</Link>
