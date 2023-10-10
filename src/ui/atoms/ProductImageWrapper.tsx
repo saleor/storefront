36cfbdfb8ac8bf17537f6ffb3c45570690b93b05
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-10 17:24:54 +0200

    fix: fix tile size (#920)
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- src/ui/atoms/ProductImageWrapper.tsx
+++ src/ui/atoms/ProductImageWrapper.tsx
@@ -2,8 +2,8 @@ import NextImage, { type ImageProps } from "next/image";
 
 export const ProductImageWrapper = (props: ImageProps) => {
 	return (
-		<div className="overflow-hidden rounded-md border">
-			<NextImage {...props} className="h-full w-full object-cover object-center p-2" />
+		<div className="aspect-square overflow-hidden rounded-md border">
+			<NextImage {...props} className="h-full w-full object-contain object-center p-2" />
 		</div>
 	);
 };
