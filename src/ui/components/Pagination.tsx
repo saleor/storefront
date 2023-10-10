866e8aaf93d3d4a9aa6e1b3d233e485b527934a4
Timur Carpeev <timuric@users.noreply.github.com>
2023-10-10 15:22:18 +0200

    Small tweaks (#916)

--- src/ui/components/Pagination.tsx
+++ src/ui/components/Pagination.tsx
@@ -10,12 +10,12 @@ export async function Pagination({
 	};
 }) {
 	return (
-		<nav className="flex items-center justify-center gap-x-4 border-t border-slate-200 px-4">
+		<nav className="flex items-center justify-center gap-x-4 border-slate-200 px-4">
 			<Link
 				href={pageInfo.hasNextPage ? `/products?cursor=${pageInfo.endCursor}` : "#"}
 				className={clsx("px-4 py-2 text-sm font-medium ", {
-					"hover:bg-slate-100": pageInfo.hasNextPage,
-					"cursor-not-allowed text-slate-400": !pageInfo.hasNextPage,
+					"hover:bg-gray-800 rounded text-gray-50 bg-gray-900": pageInfo.hasNextPage,
+					"cursor-not-allowed border rounded text-gray-400": !pageInfo.hasNextPage,
 				})}
 				aria-disabled={!pageInfo.hasNextPage}
 			>
