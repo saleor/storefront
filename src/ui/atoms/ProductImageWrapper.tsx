39b9d27f32a44e3362b50a60e4f9f093a64a9e05
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-10 14:28:56 +0200

    fix: move aspect ratio class from image to image container

--- src/ui/atoms/ProductImageWrapper.tsx
+++ src/ui/atoms/ProductImageWrapper.tsx
@@ -2,8 +2,8 @@ import NextImage, { type ImageProps } from "next/image";
 
 export const ProductImageWrapper = (props: ImageProps) => {
 	return (
-		<div className="overflow-hidden rounded-md border bg-slate-50">
-			<NextImage {...props} className="aspect-square h-full w-full object-contain object-center p-4" />
+		<div className="aspect-square overflow-hidden rounded-md border bg-slate-50">
+			<NextImage {...props} className="h-full w-full object-contain object-center p-4" />
 		</div>
 	);
 };
