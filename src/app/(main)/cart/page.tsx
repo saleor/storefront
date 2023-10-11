e75df2506d221cbaa9bfccfbfc1e55c7e1896a62
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-11 14:51:22 +0200

    feat/refactor: add ability to remove lines from cart, refactor cart styles

--- src/app/(main)/cart/page.tsx
+++ src/app/(main)/cart/page.tsx
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import Image from "next/image";
+import Link from "next/link";
 import { CheckoutLink } from "./CheckoutLink";
+import { DeleteLineButton } from "./DeleteLineButton";
 import * as Checkout from "@/lib/checkout";
 import { formatMoney } from "@/lib/graphql";
 
@@ -23,34 +25,38 @@ export default async function Page() {
 					<ul role="list" className="divide-y divide-gray-200 border-b border-t border-gray-200">
 						{lines.map((item) => (
 							<li key={item.id} className="flex py-4">
-								<div className="flex-shrink-0 rounded-md border bg-gray-50">
+								<div className="aspect-square h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border bg-gray-50 sm:h-32 sm:w-32">
 									{item.variant?.product?.thumbnail?.url && (
 										<Image
 											src={item.variant.product.thumbnail.url}
 											alt={item.variant.product.thumbnail.alt ?? ""}
 											width={200}
 											height={200}
-											className="h-24 w-24 rounded-lg object-cover object-center sm:h-32 sm:w-32"
+											className="h-full w-full object-contain object-center"
 										/>
 									)}
 								</div>
 
-								<div className="relative ml-4 flex flex-1 flex-col justify-center">
-									<div>
-										<div className="flex justify-between">
-											<div className="">
+								<div className="relative flex flex-1 flex-col justify-between p-4 py-2">
+									<div className="flex justify-between justify-items-start gap-4">
+										<div className="">
+											<Link href={`/products/${item.variant.product.slug}?variant=${item.variant.id}`}>
 												<h3 className="font-medium text-gray-700">{item.variant?.product?.name}</h3>
-												<p className="mt-1 text-sm text-gray-500">{item.variant?.product?.category?.name}</p>
-											</div>
-
-											<p className="p-4 text-right font-semibold text-gray-900">
-												{formatMoney(item.totalPrice.gross.amount, item.totalPrice.gross.currency)}
-											</p>
+											</Link>
+											<p className="mt-1 text-sm text-gray-500">{item.variant?.product?.category?.name}</p>
+											{Boolean(item.variant.name) && (
+												<p className="mt-1 text-sm text-gray-500">Variant: {item.variant.name}</p>
+											)}
 										</div>
 
-										<div className="mt-4">
-											<div className="text-sm font-bold">{item.quantity}</div>
-										</div>
+										<p className="text-right font-semibold text-gray-900">
+											{formatMoney(item.totalPrice.gross.amount, item.totalPrice.gross.currency)}
+										</p>
+									</div>
+
+									<div className="flex justify-between">
+										<div className="text-sm font-bold">Qty: {item.quantity}</div>
+										<DeleteLineButton checkoutId={checkoutId} lineId={item.id} />
 									</div>
 								</div>
 							</li>
