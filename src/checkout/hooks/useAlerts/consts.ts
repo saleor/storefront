74039f53445748375f564549c027783a4fe68453
timur <timuric@gmail.com>
2023-10-11 10:00:36 +0200

    Swap slate with gray

--- src/checkout/hooks/useAlerts/consts.ts
+++ src/checkout/hooks/useAlerts/consts.ts
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import { type TypeOptions } from "react-toastify";
 
 export const alertsContainerProps = {
-	toastClassName: "rounded shadow-none mb-2 p-0 flex font-sans text-base text-slate-900 min-h-0 bg-white",
+	toastClassName: "rounded shadow-none mb-2 p-0 flex font-sans text-base text-gray-900 min-h-0 bg-white",
 	bodyClassName: (data?: { type?: TypeOptions }) =>
 		clsx("flex w-full items-start px-5 py-3", {
 			["bg-red-400"]: data?.type === "error",
