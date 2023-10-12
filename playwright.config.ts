2c8f7eac0516325e1550917aca10b278ca4465f3
Michal Miszczyszyn <michal@mmiszy.pl>
2023-10-12 10:26:57 +0200

    temporarily disable mobile tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? [["html"], ["github"]] : [["html"], ["list"]],
+	reporter: process.env.CI ? [["html"], ["github"], ["list"]] : [["html"], ["list"]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
@@ -41,21 +41,21 @@ export default defineConfig({
 			use: { ...devices["Desktop Chrome"] },
 		},
 
-		{
-			name: "Desktop Safari",
-			use: { ...devices["Desktop Safari"] },
-		},
+		// {
+		// 	name: "Desktop Safari",
+		// 	use: { ...devices["Desktop Safari"] },
+		// },
 
-		{
-			name: "Mobile Chrome",
-			use: {
-				...devices["Pixel 5"],
-			},
-		},
-		{
-			name: "Mobile Safari",
-			use: devices["iPhone 12 Mini"],
-		},
+		// {
+		// 	name: "Mobile Chrome",
+		// 	use: {
+		// 		...devices["Pixel 5"],
+		// 	},
+		// },
+		// {
+		// 	name: "Mobile Safari",
+		// 	use: devices["iPhone 12 Mini"],
+		// },
 	],
 
 	webServer: {
