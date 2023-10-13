92de7e707934fe181e87ffcc159667f87a8d0897
Timur Carpeev <timuric@users.noreply.github.com>
2023-10-13 17:05:37 +0200

    Change to 3 columns (#949)

--- src/ui/components/ProductsList.tsx
+++ src/ui/components/ProductsList.tsx
@@ -6,10 +6,10 @@ export const ProductsList = ({ products }: { products: readonly ProductListItemF
 		<ul
 			role="list"
 			data-testid="ProductsList"
-			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"
+			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3"
 		>
 			{products.map((product, index) => (
-				<ProductElement key={product.id} product={product} loading={index < 4 ? "eager" : "lazy"} />
+				<ProductElement key={product.id} product={product} loading={index < 3 ? "eager" : "lazy"} />
 			))}
 		</ul>
 	);
