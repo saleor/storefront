c8cba7cc6106dbee609591d441242a91cf313267
Michał Miszczyszyn <michal@mmiszy.pl>
2023-10-13 11:07:03 +0200

    Add secure and httpOnly flags (#942)

--- src/app/(main)/products/[slug]/page.tsx
+++ src/app/(main)/products/[slug]/page.tsx
@@ -11,6 +11,9 @@ import { CheckoutAddLineDocument, ProductDetailsDocument, ProductListDocument } 
 import * as Checkout from "@/lib/checkout";
 import { AvailabilityMessage } from "@/ui/components/AvailabilityMessage";
 
+const shouldUseHttps =
+	process.env.NEXT_PUBLIC_STOREFRONT_URL?.startsWith("https") || !!process.env.NEXT_PUBLIC_VERCEL_URL;
+
 export async function generateMetadata({
 	params,
 	searchParams,
@@ -84,7 +87,11 @@ export default async function Page(props: { params: { slug: string }; searchPara
 			const { checkoutCreate } = await Checkout.create();
 
 			if (checkoutCreate && checkoutCreate?.checkout?.id) {
-				cookies().set("checkoutId", checkoutCreate.checkout?.id);
+				cookies().set("checkoutId", checkoutCreate.checkout?.id, {
+					secure: shouldUseHttps,
+					sameSite: "lax",
+					httpOnly: true,
+				});
 
 				checkoutId = checkoutCreate.checkout.id;
 			}
