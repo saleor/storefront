cd5983c5ffa3f415c275c9b1caaeeeff96839494
Aryan Pandey <margiaryan@gmail.com>
2023-10-14 11:59:06 +0530

    fix: Add placeholder for ACME in checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -15,6 +15,11 @@ export default function CheckoutPage({ searchParams }: { searchParams: { checkou
 	return (
 		<div className="checkout-bg min-h-[calc(100vh-106px)]">
 			<section className="mx-auto max-w-7xl p-8">
+				<div className="flex items-center font-bold">
+					<a aria-label="homepage" href="/">
+						ACME
+					</a>
+				</div>
 				<h1 className="mt-8 text-3xl font-bold text-neutral-900">Checkout</h1>
 
 				<section className="mb-12 mt-6">
