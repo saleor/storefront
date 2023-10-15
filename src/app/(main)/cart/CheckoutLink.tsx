d6af1d515d56a1e1a4df01f80aab6e5fc9508526
Aryan Pandey <margiaryan@gmail.com>
2023-10-15 19:18:32 +0530

    fix: Back Button In Checkout

--- src/app/(main)/cart/CheckoutLink.tsx
+++ src/app/(main)/cart/CheckoutLink.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Link from "next/link";
-
 type Props = {
 	disabled?: boolean;
 	checkoutId?: string;
@@ -9,13 +7,13 @@ type Props = {
 
 export const CheckoutLink = ({ disabled, checkoutId }: Props) => {
 	return (
-		<Link
+		<a
 			aria-disabled={disabled}
 			onClick={(e) => disabled && e.preventDefault()}
 			href={`/checkout?checkout=${checkoutId}`}
 			className="w-full rounded border border-transparent bg-neutral-900 px-6 py-3 text-center font-medium text-neutral-50 hover:bg-neutral-800 aria-disabled:cursor-not-allowed aria-disabled:bg-neutral-500 sm:col-start-2"
 		>
 			Checkout
-		</Link>
+		</a>
 	);
 };
