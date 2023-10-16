2adab6dddf5963998ec2f8c8130e04e2b9d6eb2a
timur <timuric@gmail.com>
2023-10-16 15:33:35 +0200

    Change thumbnails to gray

--- src/ui/atoms/ProductImageWrapper.tsx
+++ src/ui/atoms/ProductImageWrapper.tsx
@@ -2,7 +2,7 @@ import NextImage, { type ImageProps } from "next/image";
 
 export const ProductImageWrapper = (props: ImageProps) => {
 	return (
-		<div className="aspect-square overflow-hidden rounded-md border">
+		<div className="aspect-square overflow-hidden bg-neutral-50">
 			<NextImage {...props} className="h-full w-full object-contain object-center p-2" />
 		</div>
 	);
