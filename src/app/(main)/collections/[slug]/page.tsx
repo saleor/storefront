12cc8a30ae95408b6ac62dcba0df28d9d318b924
Michal Miszczyszyn <michal@mmiszy.pl>
2023-10-16 10:50:06 +0200

    Fix broken build

--- src/app/(main)/collections/[slug]/page.tsx
+++ src/app/(main)/collections/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 import { type Metadata } from "next";
 import { ProductListByCollectionDocument } from "@/gql/graphql";
 import { executeGraphQL } from "@/lib/graphql";
-import { ProductsList } from "@/ui/components/ProductsList";
+import { ProductList } from "@/ui/components/ProductList";
 
 export const generateMetadata = async ({ params }: { params: { slug: string } }): Promise<Metadata> => {
 	const { collection } = await executeGraphQL(ProductListByCollectionDocument, {
@@ -32,7 +32,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
 	return (
 		<div className="mx-auto max-w-7xl p-8 pb-16">
 			<h1 className="pb-8 text-xl font-semibold">{name}</h1>
-			<ProductsList products={products.edges.map((e) => e.node)} />
+			<ProductList products={products.edges.map((e) => e.node)} />
 		</div>
 	);
 }
