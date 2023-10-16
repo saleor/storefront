e9c6bda9cfef94dfdb5cb6158c91bcd98eceddea
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-16 10:44:35 +0200

    fix: remove redundant component, fix typo (#957)

--- src/ui/components/ProductList.tsx
+++ src/ui/components/ProductList.tsx
@@ -1,20 +1,19 @@
 import { ProductElement } from "./ProductElement";
 import { type ProductListItemFragment } from "@/gql/graphql";
 
-type Props = {
-	products: ProductListItemFragment[];
-};
-
-export const ProductList = ({ products }: Props) => {
+export const ProductList = ({ products }: { products: readonly ProductListItemFragment[] }) => {
 	return (
-		<ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4" role="list">
+		<ul
+			role="list"
+			data-testid="ProductList"
+			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3"
+		>
 			{products.map((product, index) => (
 				<ProductElement
 					key={product.id}
 					product={product}
-					//Prioritize first image in the list to speed up above the fold image loading on mobile
 					priority={index === 0}
-					loading={index < 4 ? "eager" : "lazy"}
+					loading={index < 3 ? "eager" : "lazy"}
 				/>
 			))}
 		</ul>
