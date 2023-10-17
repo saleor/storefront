810a6578417784be0dc3b1350e1a2605f73acf7f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-17 12:24:03 +0200

    fix: increase the spacing between icons in nav on the desktop viewport

--- src/ui/components/nav/Nav.tsx
+++ src/ui/components/nav/Nav.tsx
@@ -6,7 +6,7 @@ import { MobileMenu } from "./components/MobileMenu";
 
 export const Nav = () => {
 	return (
-		<nav className="flex w-full gap-4" aria-label="Main navigation">
+		<nav className="flex w-full gap-4 lg:gap-8" aria-label="Main navigation">
 			<ul className="hidden gap-4 overflow-x-auto whitespace-nowrap md:flex lg:gap-8 lg:px-0">
 				<NavLinks />
 			</ul>
