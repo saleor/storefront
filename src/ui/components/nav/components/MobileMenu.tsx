1e4990cb3e91a8971c352ed8ecccef0d0e8bc07a
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-17 12:15:09 +0200

    fix: improve MobileMenu styles

--- src/ui/components/nav/components/MobileMenu.tsx
+++ src/ui/components/nav/components/MobileMenu.tsx
@@ -26,12 +26,12 @@ export const MobileMenu = ({ children }: Props) => {
 						</div>
 						<Transition.Child
 							as={Fragment}
-							enter="transition-all duration-300"
-							enterFrom="motion-safe:opacity-0 motion-safe:-translate-y-3"
+							enter="motion-safe:transition-all motion-safe:duration-300"
+							enterFrom="opacity-0 -translate-y-3"
 							enterTo="opacity-100 translate-y-0"
-							leave="transition-all duration-300"
-							leaveFrom="opacity-100"
-							leaveTo="opacity-0"
+							leave="motion-safe:transition-all motion-safe:duration-300"
+							leaveFrom="opacity-100 translate-y-0"
+							leaveTo="opacity-0 -translate-y-3"
 						>
 							<ul
 								className="flex flex-col divide-y divide-neutral-200 whitespace-nowrap p-3 pt-0 sm:p-8 sm:pt-0 [&>li]:py-3"
