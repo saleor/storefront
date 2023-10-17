3c7dc9f3dad5240d0f9378c6311e12e3d38cd14d
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-10-17 10:35:57 +0200

    fix/refactor: improve cart link

--- src/ui/components/CartNavItem.tsx
+++ src/ui/components/CartNavItem.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { ShoppingBagIcon } from "lucide-react";
 import Link from "next/link";
+import clsx from "clsx";
 import * as Checkout from "@/lib/checkout";
 
 export const CartNavItem = async () => {
@@ -10,10 +11,21 @@ export const CartNavItem = async () => {
 	const lineCount = checkout ? checkout.lines.reduce((result, line) => result + line.quantity, 0) : 0;
 
 	return (
-		<Link href="/cart" className="flex w-12 items-center">
-			<ShoppingBagIcon className="h-6 w-6 flex-shrink-0 " aria-hidden="true" />
-			<span className="ml-2 min-w-[2ch] text-sm font-medium">{lineCount > 0 && lineCount}</span>
-			<span className="sr-only">items in cart, view bag</span>
+		<Link href="/cart" className="relative flex items-center">
+			<ShoppingBagIcon className="h-6 w-6 flex-shrink-0" aria-hidden="true" />
+			{lineCount > 0 ? (
+				<div
+					className={clsx(
+						"absolute bottom-0 right-0 -mb-2 -mr-2 flex h-4 flex-col items-center justify-center rounded bg-neutral-900 text-xs font-medium text-white",
+						lineCount > 9 ? "w-[3ch]" : "w-[2ch]",
+					)}
+				>
+					{lineCount}
+					<span className="sr-only">items in cart, view bag</span>
+				</div>
+			) : (
+				<span className="sr-only">cart</span>
+			)}
 		</Link>
 	);
 };
