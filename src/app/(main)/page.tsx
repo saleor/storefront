a89c79d296f50ba04f6b325491277bd919e49cb9
zaiste <oh@zaiste.net>
2023-10-18 11:38:22 +0200

    Improve the meta description for the home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,8 +3,9 @@ import { executeGraphQL } from "@/lib/graphql";
 import { ProductList } from "@/ui/components/ProductList";
 
 export const metadata = {
-	title: "Saleor Storefront example",
-	description: "Starter pack for building performant e-commerce experiences with Saleor.",
+	title: "ACME Storefront, powered by Saleor & Next.js",
+	description:
+		"Storefront Next.js Example for building performant e-commerce experiences with Saleor - the composable, headless commerce platform for global brands.",
 };
 
 export default async function Page() {
