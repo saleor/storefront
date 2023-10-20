c061fcc70035ee77c5d7635a6ba7a3acfcd6f7eb
KriteshTimsina <kriteshtimsina55@gmail.com>
2023-10-20 08:26:55 +0545

    fix:#903

--- src/checkout/views/EmptyCartPage/EmptyCartPage.tsx
+++ src/checkout/views/EmptyCartPage/EmptyCartPage.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { emptyCartLabels, emptyCartMessages } from "./messages";
 import { useFormattedMessages } from "@/checkout/hooks/useFormattedMessages";
 import { Button, Title } from "@/checkout/components";
+import { useRouter } from "next/navigation";
 
 export const EmptyCartPage = () => {
 	const formatMessage = useFormattedMessages();
+	const router = useRouter();
 
 	// eslint-disable-next-line no-restricted-globals
-	const goBack = () => history.back();
+	const goBack = () => router.replace("/");
 
 	return (
-		<div className="flex w-full flex-row justify-center lg:mt-10">
-			<div className="flex flex-col justify-start rounded-lg border border-neutral-400 py-6">
+		<div className="flex h-40 w-full flex-row justify-center lg:mt-10">
+			<div className="flex flex-col justify-start rounded-lg border border-neutral-400 p-6">
 				<Title>{formatMessage(emptyCartMessages.emptyCart)}</Title>
 				<p>{formatMessage(emptyCartMessages.addToCardToContinue)}</p>
 				<Button
