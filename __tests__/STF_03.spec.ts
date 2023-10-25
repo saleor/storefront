e13cf584776b473a7b2141667286467441233110
Michał Miszczyszyn <michal@mmiszy.pl>
2023-10-25 17:10:58 +0200

    test cases STF_02 and STF_03 (#989)

--- __tests__/STF_03.spec.ts
+++ __tests__/STF_03.spec.ts
@@ -7,7 +7,7 @@ import {
 	selectRandomAvailableVariant,
 } from "./utils";
 
-test("STF_01: Add items to the basket", async ({ page }) => {
+test("STF_03: Check if price are calculating correctly", async ({ page }) => {
 	await page.goto("/");
 
 	const product = await clickOnRandomProductElement({ page });
@@ -18,11 +18,14 @@ test("STF_01: Add items to the basket", async ({ page }) => {
 	await expect(page.getByTestId("CartNavItem")).toContainText("0 items");
 	await addCurrentProductToCart({ page });
 	await expect(page.getByTestId("CartNavItem")).toContainText("1 item");
+	await addCurrentProductToCart({ page });
+	await expect(page.getByTestId("CartNavItem")).toContainText("2 items");
 
 	await openCart({ page });
 
+	const totalPrice = (price * 2).toFixed(2);
 	await expect(page.getByTestId("CartProductList").getByRole("listitem")).toHaveCount(1);
 	await expect(page.getByTestId("CartProductList").getByRole("listitem")).toContainText(product.name);
-	await expect(page.getByTestId("CartProductList").getByRole("listitem")).toContainText(`Qty: 1`);
-	await expect(page.getByTestId("CartProductList").getByRole("listitem")).toContainText(price.toFixed(2));
+	await expect(page.getByTestId("CartProductList").getByRole("listitem")).toContainText(`Qty: 2`);
+	await expect(page.getByTestId("CartProductList").getByRole("listitem")).toContainText(totalPrice);
 });
