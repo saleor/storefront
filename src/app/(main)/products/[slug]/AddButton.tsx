067d063ad387fc699b0dd841437fa62f1d7128e2
zaiste <oh@zaiste.net>
2023-10-26 19:55:26 +0200

    Fix `experimantal` useFormStatus

--- src/app/(main)/products/[slug]/AddButton.tsx
+++ src/app/(main)/products/[slug]/AddButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 export function AddButton({ disabled }: { disabled?: boolean }) {
 	const { pending } = useFormStatus();
