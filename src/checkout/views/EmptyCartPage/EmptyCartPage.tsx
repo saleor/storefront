3bc6641f60b159f99ba1e7b0a2f967d1a2614263
Jakub Neander <oh@zaiste.net>
2023-10-26 12:07:07 +0200

    redirect to storefront without Next.js (#993)
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- src/checkout/views/EmptyCartPage/EmptyCartPage.tsx
+++ src/checkout/views/EmptyCartPage/EmptyCartPage.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import { Button, Title } from "@/checkout/components";
+import { Title } from "@/checkout/components";
 
 export const EmptyCartPage = () => {
-	// eslint-disable-next-line no-restricted-globals
-	const goBack = () => history.back();
-
 	return (
 		<div className="flex w-full flex-row justify-center lg:mt-10">
 			<div className="flex flex-col justify-start rounded-lg border border-neutral-400 py-6">
 				<Title>Your cart is empty</Title>
 				<p>Add anything to the cart to continue</p>
-				<Button
-					className="mt-3 md:self-end"
-					ariaLabel="Go back to store"
-					onClick={goBack}
-					variant="secondary"
-					label="Go back to store"
-				/>
+				<a className="mt-3 md:self-end" href="/">
+					Go back to store
+				</a>
 			</div>
 		</div>
 	);
