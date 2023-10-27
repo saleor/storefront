5db03c14ef68af9bae05a1159dad167948b5790f
Igor Swatowski <igorswatowski@gmail.com>
2023-10-27 12:22:07 +0200

    Fix

--- apps/storefront/components/AdvantagesBlock/AdvantagesBlockItem.tsx
+++ apps/storefront/components/AdvantagesBlock/AdvantagesBlockItem.tsx
@@ -12,8 +12,12 @@ export const AdvantagesBlockItem: React.FC<AdvantagesBlockItemProps> = ({ title,
       <div className="mb-6 bg-white p-4 rounded-xl shadow-md w-16 h-16">
         <Image src={image} alt="" />
       </div>
-      <h3 className="mb-2 font-semibold text-[24px] text-black text-center">{title}</h3>
-      <p className="text-[18px] font-medium text-[#4F4F4F] text-center">{text}</p>
+      <h3 className="mb-2 font-semibold text-[20px] md:text-[24px] lg:text-[24px] xl:text-[24px] text-black text-center">
+        {title}
+      </h3>
+      <p className="text-sm md:text-[18px] lg:text-[18px] xl:text-[18px] font-medium text-[#4F4F4F] text-center">
+        {text}
+      </p>
     </div>
   );
 };
