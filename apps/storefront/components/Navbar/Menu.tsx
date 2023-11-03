2f89a0c9e111c2ce89df6abc7dc97218914ef8c6
Igor Swatowski <igorswatowski@gmail.com>
2023-11-03 14:59:04 +0100

    Refactor menu component

--- apps/storefront/components/Navbar/Menu.tsx
+++ apps/storefront/components/Navbar/Menu.tsx
@@ -8,50 +8,47 @@ import styles from "./Navbar.module.css";
 import { STOREFRONT_NAME } from "@/lib/const";
 
 export type MenuItem = {
+  id: string;
   name: string;
 };
 
+const MAX_MENU_ITEMS = 5;
+const excludedCategories = ["Mix", "Hurt", "Detal"];
+
+const isExcludedCategory = (item: MenuItem): boolean => excludedCategories.includes(item.name);
+
 export function Menu() {
   const { query } = useRegions();
-
-  const { error, data } = useMainMenuQuery({
-    variables: { ...query },
-  });
+  const { error, data } = useMainMenuQuery({ variables: { ...query } });
 
   if (error) {
     console.error("Navbar/Menu component error", error.message);
+    return null;
   }
 
   const menuItems = data?.menu?.items || [];
 
-  const maxMenuItems = 5;
-  const slicedMenuItems = menuItems.slice(0, maxMenuItems);
-
-  const isExcludedCategory = (item: MenuItem): boolean =>
-    item.name === "Mix" || item.name === "Hurt" || item.name === "Detal";
-
-  const visibleCategoryOnMenu =
+  const visibleMenuItems =
     STOREFRONT_NAME === "FASHION4YOU"
       ? menuItems.filter((item) => !isExcludedCategory(item))
-      : slicedMenuItems;
+      : menuItems.slice(0, MAX_MENU_ITEMS);
 
   return (
     <nav className={styles.nav}>
       <ol>
-        {visibleCategoryOnMenu.map((item) => {
+        {visibleMenuItems.map((item) => {
           if (item.name === "Aktualności" || item.name === "News") {
             return (
               <li key={item.id}>
                 <DropdownMenu menuItem={item} isNews />
               </li>
             );
-          } else {
-            return (
-              <li key={item?.id}>
-                <DropdownMenu menuItem={item} />
-              </li>
-            );
           }
+          return (
+            <li key={item?.id}>
+              <DropdownMenu menuItem={item} />
+            </li>
+          );
         })}
       </ol>
     </nav>
