846a6e012421fa5cc5f02036da623b4000470bc7
zaiste <oh@zaiste.net>
2023-11-10 14:36:42 +0100

    Add order list for logged user

--- src/app/(main)/login/LoginComponent.tsx
+++ src/app/(main)/login/LoginComponent.tsx
@@ -2,6 +2,7 @@
 
 import { useQuery } from "urql";
 import { useSaleorAuthContext } from "@saleor/auth-sdk/react";
+import Link from "next/link";
 import { CurrentUserDocument, type CurrentUserQuery } from "@/gql/graphql";
 import { LoginForm } from "@/ui/components/LoginForm";
 import { UserCard } from "@/ui/components/UserCard";
@@ -16,13 +17,18 @@ export const LoginComponent = () => {
 	return data?.me ? (
 		<>
 			<UserCard user={data.me} />
-			<button
-				onClick={() => signOut()}
-				className="rounded bg-neutral-800 px-4 py-2 text-neutral-200 hover:bg-neutral-700"
-				type="button"
-			>
-				Log Out
-			</button>
+			<div className="mt-4 flex space-x-2">
+				<Link href="/orders" className="h-full rounded bg-neutral-100 px-4 py-2 hover:bg-neutral-200">
+					My orders
+				</Link>
+				<button
+					onClick={() => signOut()}
+					className="rounded bg-neutral-800 px-4 py-2 text-neutral-200 hover:bg-neutral-700"
+					type="button"
+				>
+					Log Out
+				</button>
+			</div>
 		</>
 	) : (
 		<LoginForm />
