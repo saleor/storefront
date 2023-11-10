3606dc9f15c9d54c2705858dd0b88156d718c644
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-11-10 19:55:03 +0100

    fix/feat: move repeating code to the component

--- src/checkout/views/EmptyCartPage/EmptyCartPage.tsx
+++ src/checkout/views/EmptyCartPage/EmptyCartPage.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Title } from "@/checkout/components";
 import { LinkAsButton } from "@/checkout/components/LinkAsButton";
+import { ErrorContentWrapper } from "@/checkout/components/ErrorContentWrapper";
 
 export const EmptyCartPage = () => {
 	return (
-		<div className="mx-auto flex max-w-screen-sm flex-col items-center gap-y-4 bg-neutral-50 px-8 py-16 text-center">
+		<ErrorContentWrapper>
 			<Title className="mb-0 text-xl">Your cart is empty</Title>
 			<p>Add anything to the cart to continue.</p>
 			<LinkAsButton href="/" variant="secondary">
 				Go back to store
 			</LinkAsButton>
-		</div>
+		</ErrorContentWrapper>
 	);
 };
--- src/checkout/views/PageNotFound/PageNotFound.tsx
+++ src/checkout/views/PageNotFound/PageNotFound.tsx
@@ -1,6 +1,7 @@
 import { type FallbackProps } from "react-error-boundary";
 import { SaleorLogo } from "@/checkout/assets/images/SaleorLogo";
 import { Button } from "@/checkout/components/Button";
+import { ErrorContentWrapper } from "@/checkout/components/ErrorContentWrapper";
 
 export const PageNotFound = ({ error }: Partial<FallbackProps>) => {
 	console.error(error);
@@ -9,12 +10,12 @@ export const PageNotFound = ({ error }: Partial<FallbackProps>) => {
 	const goBack = () => history.back();
 
 	return (
-		<div className="mx-auto flex max-w-screen-sm flex-col items-center gap-y-4 bg-neutral-50 px-8 py-16 text-center">
+		<ErrorContentWrapper>
 			<div className="mb-4 flex w-28 flex-col">
 				<SaleorLogo />
 			</div>
 			<p>We couldn&apos;t fetch information about your checkout. Go back to the store and try again.</p>
 			<Button ariaLabel="Go back to store" onClick={goBack} variant="secondary" label="Go back to store" />
-		</div>
+		</ErrorContentWrapper>
 	);
 };
