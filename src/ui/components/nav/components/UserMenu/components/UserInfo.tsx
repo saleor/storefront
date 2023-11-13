96092e7b396ec8cb0e872ae6b92dfc7386eb0c2f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-11-13 12:33:08 +0100

    fix: fix condition in UserInfo component

--- src/ui/components/nav/components/UserMenu/components/UserInfo.tsx
+++ src/ui/components/nav/components/UserMenu/components/UserInfo.tsx
@@ -5,16 +5,11 @@ type Props = {
 };
 
 export const UserInfo = ({ user }: Props) => {
-	const userName =
-		user.firstName && user.lastName ? `${user.firstName.slice(0, 1)}${user.lastName.slice(0, 1)}` : null;
+	const userName = user.firstName && user.lastName ? `${user.firstName} ${user.lastName}` : null;
 
 	return (
 		<p className="truncate px-5 py-2 text-xs text-neutral-700">
-			{userName && (
-				<span className="mb-0.5 block truncate font-bold">
-					{user.firstName} {user.lastName}
-				</span>
-			)}
+			{userName && <span className="mb-0.5 block truncate font-bold">{userName}</span>}
 			{user.email}
 		</p>
 	);
