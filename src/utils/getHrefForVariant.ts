df25129471fbf7b06d8784ddcb9b0a75d3b37ffc
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-11-15 13:48:50 +0100

    Update src/utils/getHrefForVariant.ts
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- src/utils/getHrefForVariant.ts
+++ src/utils/getHrefForVariant.ts
@@ -1,9 +1,4 @@
-type Args = {
-	productSlug: string;
-	variantId: string;
-};
-
-export function getHrefForVariant({ productSlug, variantId }: Args): string {
+export function getHrefForVariant({ productSlug, variantId }: { productSlug: string; variantId: string }): string {
 	const pathname = `/products/${encodeURIComponent(productSlug)}`;
 	const query = new URLSearchParams({ variant: variantId });
 	return `${pathname}?${query.toString()}`;
