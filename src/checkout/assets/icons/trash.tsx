2999cbcb3a9ac49339cd5b1640d95c2afe9a297c
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-11-17 00:03:44 +0100

    fix: fix checkout svg icons

--- src/checkout/assets/icons/trash.tsx
+++ src/checkout/assets/icons/trash.tsx
@@ -4,8 +4,8 @@ export function TrashIcon() {
 			<path
 				d="M5 5L6 22H18L19 5M5 5H9M5 5H3M19 5H15M19 5H21M9 5L10 2H14L15 5M9 5H15"
 				stroke="#394052"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	);
