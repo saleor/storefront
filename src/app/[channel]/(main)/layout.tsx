e722556f4c11fbf96de5c5a655a2709b48269dea
mhcassiem <mhcassiem@gmail.com>
2024-04-03 13:53:07 +0200

    styling, use new component

--- src/app/[channel]/(main)/layout.tsx
+++ src/app/[channel]/(main)/layout.tsx
@@ -1,7 +1,7 @@
 import { type ReactNode } from "react";
-import NextImage from "next/image";
 import { Footer } from "@/ui/components/Footer";
 import { Header } from "@/ui/components/Header";
+import { BackgroundImageWrapper } from "@/ui/atoms/BackgroundImageWrapper";
 
 export const metadata = {
 	title: "Saleor Storefront example",
@@ -13,12 +13,7 @@ export default function RootLayout(props: { children: ReactNode; params: { chann
 		<>
 			<Header channel={props.params.channel} />
 			<div className="relative flex min-h-[calc(100dvh-64px)] flex-col">
-				<NextImage
-					fill={true}
-					className="absolute h-full w-full opacity-80"
-					src={process.env.SITE_BACKGROUND!}
-					alt=""
-				/>
+				<BackgroundImageWrapper className="h-auto w-full" alt="Background" src={"null"} />
 				<main className="relative flex-1 text-neutral-300">{props.children}</main>
 				<Footer channel={props.params.channel} />
 			</div>
