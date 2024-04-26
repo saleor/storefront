1709629cbe171996aa5ee6f2c697b7409f390189
mhcassiem <mhcassiem@gmail.com>
2024-04-26 12:51:32 +0200

    remove backgroundimage component move logo

--- src/app/[channel]/(main)/layout.tsx
+++ src/app/[channel]/(main)/layout.tsx
@@ -1,7 +1,6 @@
 import { type ReactNode } from "react";
 import { Footer } from "@/ui/components/Footer";
 import { Header } from "@/ui/components/Header";
-import { BackgroundImageWrapper } from "@/ui/atoms/BackgroundImageWrapper";
 
 export const metadata = {
 	title: "Saleor Storefront example",
@@ -13,8 +12,7 @@ export default function RootLayout(props: { children: ReactNode; params: { chann
 		<>
 			<Header channel={props.params.channel} />
 			<div className="relative flex min-h-screen flex-col">
-				<BackgroundImageWrapper className="" alt="Background" src={"null"} />
-				<main className="relative flex-1 text-neutral-300">{props.children}</main>
+				<main className="backgroundImage relative flex-1 text-neutral-300">{props.children}</main>
 				<Footer channel={props.params.channel} />
 			</div>
 		</>
--- src/ui/components/Logo.tsx
+++ src/ui/components/Logo.tsx
@@ -1,24 +1,23 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 import { LinkWithChannel } from "../atoms/LinkWithChannel";
-
-const companyName = "CK";
 
 export const Logo = () => {
 	const pathname = usePathname();
 
-	if (pathname === "/") {
+	if (pathname === "/default-channel/pages/home") {
 		return (
 			<h1 className="flex items-center font-bold" aria-label="homepage">
-				{companyName}
+				<Image src={"/CK-LOGO-101-Photoroom.svg"} alt="Caspian King" height={100} width={100} />
 			</h1>
 		);
 	}
 	return (
 		<div className="flex items-center font-bold text-neutral-200">
 			<LinkWithChannel aria-label="homepage" href="/pages/home">
-				{companyName}
+				<Image src={"/CK-LOGO-101-Photoroom.svg"} alt="Caspian King" height={100} width={100} />
 			</LinkWithChannel>
 		</div>
 	);
