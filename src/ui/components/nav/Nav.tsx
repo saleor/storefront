3573f9ed347c413b5203898973610c0aa741170f
mhcassiem <mhcassiem@gmail.com>
2024-04-28 01:48:27 +0200

    remove nav overflow

--- src/ui/components/nav/Nav.tsx
+++ src/ui/components/nav/Nav.tsx
@@ -8,7 +8,7 @@ import { SearchBar } from "./components/SearchBar";
 export const Nav = ({ channel }: { channel: string }) => {
 	return (
 		<nav className="flex w-full gap-4 lg:gap-6" aria-label="Main navigation">
-			<ul className="hidden gap-4 overflow-x-auto whitespace-nowrap md:flex lg:gap-8 lg:px-0">
+			<ul className="hidden gap-4 whitespace-nowrap md:flex lg:gap-8 lg:px-0">
 				<NavLinks channel={channel} />
 			</ul>
 			<div className="ml-auto flex items-center justify-center gap-4 whitespace-nowrap lg:gap-8">
