00d3ae433bbbfa8f4935486fc0279ded7c4e745f
mass-gueye <mass-gueye@protonmail.com>
2024-07-09 10:53:13 +0000

    fixing pagination and linting

--- src/ui/components/ProductList.tsx
+++ src/ui/components/ProductList.tsx
@@ -6,7 +6,7 @@ export const ProductList = ({ products }: { products: readonly ProductListItemFr
 		<ul
 			role="list"
 			data-testid="ProductList"
-			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3"
+			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
 		>
 			{products.map((product, index) => (
 				<ProductElement
