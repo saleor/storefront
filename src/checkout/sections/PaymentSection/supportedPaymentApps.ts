f5e31cf7c64997190092fc56c7081e41aa0f8fcd
delemeator <delemeator@gazeta.pl>
2024-09-10 19:05:06 +0200

    Add dummy to supported payment apps

--- src/checkout/sections/PaymentSection/supportedPaymentApps.ts
+++ src/checkout/sections/PaymentSection/supportedPaymentApps.ts
@@ -2,8 +2,11 @@ import { AdyenDropIn } from "./AdyenDropIn/AdyenDropIn";
 import { adyenGatewayId } from "./AdyenDropIn/types";
 import { StripeComponent } from "./StripeElements/stripeComponent";
 import { stripeGatewayId } from "./StripeElements/types";
+import { DummyComponent } from "./Dummy/dummyComponent";
+import { dummyGatewayId } from "./Dummy/types";
 
 export const paymentMethodToComponent = {
 	[adyenGatewayId]: AdyenDropIn,
 	[stripeGatewayId]: StripeComponent,
+	[dummyGatewayId]: DummyComponent,
 };
