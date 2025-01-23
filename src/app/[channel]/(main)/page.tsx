bf20ac266b84544b25997566661eaffb45c2a120
Ayoub Arahmat <ayoubarahmat@gmail.com>
2025-01-23 16:25:44 +0100

    change brand name

--- src/app/[channel]/(main)/page.tsx
+++ src/app/[channel]/(main)/page.tsx
@@ -3,9 +3,9 @@ import { executeGraphQL } from "@/lib/graphql";
 import { ProductList } from "@/ui/components/ProductList";
 
 export const metadata = {
-	title: "ACME Storefront, powered by Saleor & Next.js",
+	title: "Medamall",
 	description:
-		"Storefront Next.js Example for building performant e-commerce experiences with Saleor - the composable, headless commerce platform for global brands.",
+		"Medamall  performant e-commerce - the composable, headless commerce platform for global brands.",
 };
 
 export default async function Page({ params }: { params: { channel: string } }) {
