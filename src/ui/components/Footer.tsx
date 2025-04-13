82e9ac8d799f6d810af901277221517b4381f8dd
Konstantinos Voulgaropoulos <voulkon93@gmail.com>
2025-04-13 21:30:15 +0300

    fix: Remove unused imports from Footer component

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import Image from "next/image";
 import { LinkWithChannel } from "../atoms/LinkWithChannel";
 import { ChannelSelect } from "./ChannelSelect";
 import { ChannelsListDocument, MenuGetBySlugDocument } from "@/gql/graphql";
