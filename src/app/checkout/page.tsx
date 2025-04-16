d5d9b0cb7b732e8eb8b5c8aea8b26b895d233f82
p-febis <preston.febis@yahoo.com>
2025-04-16 16:59:23 -0300

    fix anchor tag instead of Link

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { invariant } from "ts-invariant";
 import { RootWrapper } from "./pageWrapper";
 
@@ -19,9 +20,9 @@ export default async function CheckoutPage(props: {
 		<div className="min-h-dvh bg-white">
 			<section className="mx-auto flex min-h-dvh max-w-7xl flex-col p-8">
 				<div className="flex items-center font-bold">
-					<a aria-label="homepage" href="/">
+					<Link aria-label="homepage" href="/">
 						ACME
-					</a>
+					</Link>
 				</div>
 				<h1 className="mt-8 text-3xl font-bold text-neutral-900">Checkout</h1>
 
