99026bd190792b68ca242132a274e2048c5139d8
Konstantinos Voulgaropoulos <voulkon93@gmail.com>
2025-04-17 23:53:27 +0300

    last placeholder text

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -21,7 +21,7 @@ export default function CheckoutPage({
 			<section className="mx-auto flex min-h-dvh max-w-7xl flex-col p-8">
 				<div className="flex items-center font-bold">
 					<a aria-label="homepage" href="/">
-						ACME
+						Reyes Handmade Jewels
 					</a>
 				</div>
 				<h1 className="mt-8 text-3xl font-bold text-neutral-900">Checkout</h1>
