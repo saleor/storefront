40bb29e52fd97ebcdb6369cd440f77d6dd605987
droniu <droniu@droniu.dev>
2025-05-27 13:01:19 +0200

    Stripe App support

---
+++ src/checkout/sections/PaymentSection/StripeV2DropIn/types.ts
@@ -0,0 +1,2 @@
+export const stripeV2GatewayId = "saleor.app.payment.stripe-v2";
+export type StripeV2GatewayId = typeof stripeV2GatewayId;
--- src/checkout/sections/PaymentSection/supportedPaymentApps.ts
+++ src/checkout/sections/PaymentSection/supportedPaymentApps.ts
@@ -4,9 +4,12 @@ import { DummyComponent } from "./DummyDropIn/dummyComponent";
 import { dummyGatewayId } from "./DummyDropIn/types";
 import { StripeComponent } from "./StripeElements/stripeComponent";
 import { stripeGatewayId } from "./StripeElements/types";
+import { StripeComponent as StripeV2Component } from "./StripeV2DropIn/stripeComponent";
+import { stripeV2GatewayId } from "./StripeV2DropIn/types";
 
 export const paymentMethodToComponent = {
 	[adyenGatewayId]: AdyenDropIn,
 	[stripeGatewayId]: StripeComponent,
+	[stripeV2GatewayId]: StripeV2Component,
 	[dummyGatewayId]: DummyComponent,
 };
