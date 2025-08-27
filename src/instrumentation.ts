6e39af9f853be989ce12d2b5b50ddf98a15125b4
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2025-08-27 16:09:30 +0200

    Add domain attribute

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,9 +1,18 @@
-import { registerOTel } from "@vercel/otel";
+import { registerOTel, OTLPHttpProtoTraceExporter } from "@vercel/otel";
+
+const saleorEnvironmentSpanAttr = new URL(process.env.NEXT_PUBLIC_SALEOR_API_URL!).host;
 
 export function register() {
 	registerOTel({
 		serviceName: "storefront",
-
+		attributes: {
+			"saleor.environment.domain": saleorEnvironmentSpanAttr,
+		},
+		traceExporter: new OTLPHttpProtoTraceExporter({
+			headers: {
+				"x-alb-access-token": process.env.OTEL_ACCESS_TOKEN!,
+			},
+		}),
 		instrumentationConfig: {
 			fetch: {
 				propagateContextUrls: [process.env.NEXT_PUBLIC_SALEOR_API_URL!],
