d4914bea3f83eef591dcc304d454de44c2352392
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2025-09-17 11:06:32 +0200

    Modify auth token header

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,7 +3,7 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
 
 export const createBatchSpanProcessor = (args: { accessToken: string | undefined }) => {
-	const headers = args.accessToken ? { "x-alb-access-token": args.accessToken } : undefined;
+	const headers = args.accessToken ? { "x-api-key": args.accessToken } : undefined;
 
 	return new BatchSpanProcessor(
 		new OTLPTraceExporter({
