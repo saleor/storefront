3dfe9de84bd0a22964a39bdaabbcc4328bdc9cb5
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2025-12-04 22:00:35 +0100

    fix mobile menu

--- src/ui/components/nav/components/MobileMenu.tsx
+++ src/ui/components/nav/components/MobileMenu.tsx
@@ -19,14 +19,14 @@ export const MobileMenu = ({ children }: Props) => {
 			<OpenButton onClick={openMenu} aria-controls="mobile-menu" />
 			<Transition show={isOpen}>
 				<Dialog onClose={closeMenu}>
-					<Dialog.Panel className="fixed inset-0 z-20 flex h-dvh w-screen flex-col overflow-y-scroll">
+					<Dialog.Panel className="fixed inset-0 z-20 flex h-dvh w-screen flex-col overflow-y-scroll bg-white">
 						<Transition.Child
-							className="sticky top-0 z-10 flex h-16 shrink-0 bg-neutral-100/50 px-3 backdrop-blur-md sm:px-8"
+							className="sticky top-0 z-10 flex h-16 shrink-0 bg-white px-3 backdrop-blur-md sm:px-8"
 							enter="motion-safe:transition-all motion-safe:duration-150"
 							enterFrom="bg-transparent"
-							enterTo="bg-neutral-100"
+							enterTo="bg-white"
 							leave="motion-safe:transition-all motion-safe:duration-150"
-							leaveFrom="bg-neutral-100"
+							leaveFrom="bg-white"
 							leaveTo="bg-transparent"
 						>
 							<Logo />
@@ -42,7 +42,7 @@ export const MobileMenu = ({ children }: Props) => {
 							leaveTo="opacity-0 -translate-y-3 bg-transparent"
 						>
 							<ul
-								className="flex h-full flex-col divide-y divide-neutral-200 whitespace-nowrap p-3 pt-0 sm:p-8 sm:pt-0 [&>li]:py-3"
+								className="flex h-full flex-col whitespace-nowrap p-3 pt-0 sm:p-8 sm:pt-0 [&>*:nth-child(n+3)]:border-t [&>*:nth-child(n+3)]:border-neutral-200 [&>li]:py-3"
 								id="mobile-menu"
 							>
 								{children}
